73a9fe1767c966d1c0bbc8d73f856616e99b4a36
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-05-02 22:25:11 +0800

    chore: remove lib from git

--- packages/iceworks-cli/lib/command/init/generateNpmName.js
+++
@@ -1,7 +0,0 @@
-const decamelize = require('decamelize');
-
-module.exports = function(name, npmScope) {
-  // WebkitTransform -> webkit-transform
-  name = decamelize(name, '-');
-  return npmScope ? `${npmScope}/${name}` : name;
-};
--- packages/iceworks-cli/lib/utils/log.js
+++
@@ -1,9 +0,0 @@
-const npmlog = require('npmlog');
-
-const envs = ['verbose', 'info', 'error', 'warn'];
-const logLevel =
-  envs.indexOf(process.env.LOG_LEVEL) !== -1 ? process.env.LOG_LEVEL : 'info';
-
-npmlog.level = logLevel;
-
-module.exports = npmlog;
