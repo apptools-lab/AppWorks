60e9e76bbc1b07be147744f58a4ad734a531f62b
alvinhui <alvin.hui@qq.com>
2020-06-05 15:53:36 +0800

    feat: auto set npm configuration

--- packages/common-service/src/index.ts
+++ packages/common-service/src/index.ts
@@ -3,6 +3,12 @@ import * as fse from 'fs-extra';
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+export const CONFIGURATION_SECTION = 'iceworks';
+export const CONFIGURATION_KEY_PCKAGE_MANAGER = 'packageManager';
+export const CONFIGURATION_KEY_NPM_REGISTRY = 'npmRegistry';
+export const CONFIGURATION_SECTION_PCKAGE_MANAGER = `${CONFIGURATION_SECTION}.${CONFIGURATION_KEY_PCKAGE_MANAGER}`;
+export const CONFIGURATION_SECTION_NPM_REGISTRY = `${CONFIGURATION_SECTION}.${CONFIGURATION_KEY_NPM_REGISTRY}`;
+
 export async function checkIsAliInternal(): Promise<boolean> {
   const isAliInternal = await checkAliInternal();
   return isAliInternal;
@@ -16,11 +22,11 @@ export async function checkPathExists(p: string, folderName?: string): Promise<b
 }
 
 export function saveDataToSettingJson(section: string, data: any, configurationTarget: boolean = true): void {
-  vscode.workspace.getConfiguration('iceworks').update(section, data, configurationTarget);
+  vscode.workspace.getConfiguration(CONFIGURATION_SECTION).update(section, data, configurationTarget);
 }
 
 export function getDataFromSettingJson(section: string): string {
-  return vscode.workspace.getConfiguration('iceworks').get(section);
+  return vscode.workspace.getConfiguration(CONFIGURATION_SECTION).get(section);
 }
 
 export function executeCommand(...arg: any[]) {
@@ -33,7 +39,7 @@ export async function setPackageManager(packageManagers: string[]): Promise<void
   quickPick.items = packageManagers.map(label => ({ label }));
   quickPick.onDidChangeSelection(async selection => {
     if (selection[0]) {
-      await vscode.workspace.getConfiguration().update('iceworks.packageManager', selection[0].label, true);
+      saveDataToSettingJson(CONFIGURATION_KEY_PCKAGE_MANAGER, selection[0].label);
       vscode.window.showInformationMessage(`Setting ${selection[0].label} client successfully!`);
       quickPick.hide();
     }
@@ -50,9 +56,9 @@ export async function setNpmRegistry(npmRegistrys: string[]): Promise<void> {
   quickPick.onDidChangeSelection(async selection => {
     if (selection[0]) {
       if (selection[0].label === addOtherRegistryLabel) {
-        vscode.commands.executeCommand('workbench.action.openSettings', 'iceworks.npmRegistry');
+        vscode.commands.executeCommand('workbench.action.openSettings', CONFIGURATION_SECTION_NPM_REGISTRY);
       } else {
-        await vscode.workspace.getConfiguration().update('iceworks.npmRegistry', selection[0].label, true);
+        saveDataToSettingJson(CONFIGURATION_KEY_NPM_REGISTRY, selection[0].label);
         vscode.window.showInformationMessage(`Setting ${selection[0].label} Registry successfully!`);
       }
 
@@ -65,10 +71,56 @@ export async function setNpmRegistry(npmRegistrys: string[]): Promise<void> {
 
 export function getPackageManagersDefaultFromPackageJson(packageJsonPath: string): string[] {
   const packageJson = JSON.parse(fse.readFileSync(packageJsonPath, 'utf-8'));
-  return packageJson.contributes.configuration.properties['iceworks.packageManager'].enum;
+  return packageJson.contributes.configuration.properties[CONFIGURATION_SECTION_PCKAGE_MANAGER].enum;
 }
 
 export function getNpmRegistriesDefaultFromPckageJson(packageJsonPath: string): string[] {
   const packageJson = JSON.parse(fse.readFileSync(packageJsonPath, 'utf-8'));
-  return packageJson.contributes.configuration.properties['iceworks.npmRegistry'].enum;
+  return packageJson.contributes.configuration.properties[CONFIGURATION_SECTION_NPM_REGISTRY].enum;
+}
+
+export async function autoSetNpmConfiguration(globalState: vscode.Memento) {
+  const isAliInternal = await checkAliInternal();
+  autoSetPackageManagerConfiguration(globalState, isAliInternal);
+  autoSetNpmRegistryConfiguration(globalState, isAliInternal);
+}
+
+async function autoSetPackageManagerConfiguration(globalState: vscode.Memento, isAliInternal: boolean) {
+  console.log('autoSetPackageManager: run');
+
+  const stateKey = 'iceworks.packageManagerIsSeted';
+  const packageManagerIsSeted = globalState.get(stateKey);
+  if (!packageManagerIsSeted && isAliInternal) {
+    console.log('autoSetPackageManager: do');
+    saveDataToSettingJson(CONFIGURATION_KEY_PCKAGE_MANAGER, 'tnpm');
+  }
+
+  vscode.workspace.onDidChangeConfiguration(function(event: vscode.ConfigurationChangeEvent) {
+    const isTrue = event.affectsConfiguration(CONFIGURATION_SECTION_PCKAGE_MANAGER);
+    if (isTrue) {
+      console.log('autoSetPackageManager: did change');
+
+      globalState.update(stateKey, true);
+    }
+  });
+}
+
+async function autoSetNpmRegistryConfiguration(globalState: vscode.Memento, isAliInternal: boolean) {
+  console.log('autoSetNpmRegistry: run');
+
+  const stateKey = 'iceworks.npmRegistryIsSeted';
+  const npmRegistryIsSeted = globalState.get(stateKey);
+  if (!npmRegistryIsSeted && isAliInternal) {
+    console.log('autoSetNpmRegistry: do');
+    saveDataToSettingJson(CONFIGURATION_KEY_NPM_REGISTRY, 'https://registry.npm.alibaba-inc.com');
+  }
+
+  vscode.workspace.onDidChangeConfiguration(function(event: vscode.ConfigurationChangeEvent) {
+    const isTrue = event.affectsConfiguration(CONFIGURATION_SECTION_NPM_REGISTRY);
+    if (isTrue) {
+      console.log('autoSetNpmRegistry: did change');
+
+      globalState.update(stateKey, true);
+    }
+  });
 }
