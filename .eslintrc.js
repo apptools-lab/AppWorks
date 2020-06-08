847cb20a89fe9409f82a7962ac2b0180caea2b59
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-06-08 14:37:13 +0800

    chore: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ const { eslint, tslint, deepmerge } = require('@ice/spec');
 const commonRules = {
   'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.tsx'] }],
   'react/jsx-no-target-blank': [0],
-  'prefer-object-spread': 0,
-  '@typescript-eslint/array-type': 0,
-  '@typescript-eslint/no-non-null-assertion': 0
+  'prefer-object-spread': 0
 };
 
 const jsRules = deepmerge(eslint, {
@@ -24,7 +22,9 @@ const tsRules = deepmerge(tslint, {
   rules: {
     ...commonRules,
     '@typescript-eslint/ban-ts-ignore': 0,
-    '@typescript-eslint/array-type': 0
+    '@typescript-eslint/array-type': 0,
+    '@typescript-eslint/array-type': 0,
+    '@typescript-eslint/no-non-null-assertion': 0
   },
 });
 
