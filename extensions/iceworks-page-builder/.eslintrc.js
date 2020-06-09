2d8b9a991e93594c76be13fcd792ba3d0482d066
alvinhui <alvin.hui@qq.com>
2020-06-09 11:19:15 +0800

    fix: build page-builder package

--- extensions/iceworks-page-builder/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const { tslint, deepmerge } = require('@ice/spec');
-
-module.exports = deepmerge(tslint, {
-  rules: {
-    'react/jsx-filename-extension': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/array-type': 0
-  },
-});
