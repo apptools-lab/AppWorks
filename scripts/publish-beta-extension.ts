385243dd709763f630daa960ea244a7e7c9928df
yangfan <18767120422@163.com>
2020-06-11 17:57:53 +0800

    feat: update

--- scripts/publish-beta-extension.ts
+++ scripts/publish-beta-extension.ts
@@ -1,8 +1,18 @@
 /**
  * Scripts to check unpublished version and run beta publish  
  */
+import * as oss from 'ali-oss';
+import * as path from 'path';
 import { spawnSync } from 'child_process';
 import { IExtensionInfo, getExtensionInfos } from './getExtensionInfos';
+
+const ossClient = oss({
+  bucket: 'iceworks',
+  endpoint: 'oss-cn-hangzhou.aliyuncs.com',
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  accessKeySecret: process.env.ACCESS_KEY_SECRET,
+  timeout: '120s',
+});
 
 function publish(extension: string, directory: string, version: string): void {
   // npm install
@@ -14,7 +24,7 @@ function publish(extension: string, directory: string, version: string): void {
   });
 
   // vsce package
-  console.log('[VSCE] PUBLISH: ', `${extension}@${version}`);
+  console.log('[VSCE] PACKAGE: ', `${extension}@${version}`);
   spawnSync('vsce', [
     'package',
   ], {
@@ -23,7 +33,16 @@ function publish(extension: string, directory: string, version: string): void {
   });
 
   // Upload to oss
-     
+  const extensionFile = `${extension}-${version}.vsix`;
+  const extensionFilePath = path.resolve(directory, extensionFile);
+  ossClient
+    .put(`vscode-extensions/beta/${extensionFile}`, extensionFilePath)
+    .then(() => {
+      console.log(`[PUBLISH BETA] ${extensionFile} upload success.`);
+    })
+    .catch(() => {
+      console.log(`[ERROR] ${extensionFile} upload failed.`);
+    });
 }
 
 // Entry
