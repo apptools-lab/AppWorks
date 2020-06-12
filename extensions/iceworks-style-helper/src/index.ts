8f01a640b49f2329d62083183cf78d622e3112fe
yangfan <18767120422@163.com>
2020-06-12 13:42:28 +0800

    feat: remove wrong push

--- extensions/iceworks-style-helper/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-import inlineStyleAutoComplete from './inlineStyleAutoComplete';
-
-function activate(context: vscode.ExtensionContext) {
-  inlineStyleAutoComplete(context);
-};
-
-exports.activate = activate;
