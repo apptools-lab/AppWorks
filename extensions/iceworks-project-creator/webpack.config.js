2d66cab488c105e79eaf783206634887981c20a8
luhc228 <luhengchang228@126.com>
2020-06-15 18:54:24 +0800

    feat: add webpack bundle in project-creator extension

---
+++ extensions/iceworks-project-creator/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const tsConfigPath = path.join(__dirname, 'tsconfig.json');
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+    '@iceworks/vscode-webview': 'commonjs @iceworks/vscode-webview'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              configFile: tsConfigPath,
+            },
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
