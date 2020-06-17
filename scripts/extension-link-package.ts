baad14bd47450f30655c9766f1d0651a6b77e072
luhc228 <luhengchang228@126.com>
2020-06-17 14:51:14 +0800

    feat: add webview install and link deps

--- scripts/extension-link-package.ts
+++ scripts/extension-link-package.ts
@@ -7,6 +7,7 @@ import * as spawn from 'cross-spawn';
   const extensionFiles = await fse.readdir(extensionsPath);
   const packagesPath = path.join(__dirname, '../packages');;
   const packageFiles = await fse.readdir(packagesPath);
+
   return await Promise.all(extensionFiles.map(async extensionFile => {
     const cwd = path.join(extensionsPath, extensionFile);
     const cwdStat = await fse.stat(cwd);
@@ -16,15 +17,37 @@ import * as spawn from 'cross-spawn';
         const packageNames = Reflect.ownKeys(packageJSON.dependencies).filter((moduleName: string) => {
           return /^@iceworks/.test(moduleName);
         });
-
+        // link packages to extension
         if (packageNames.length) {
           packageNames.forEach((packageName: string) => {
             packageName = packageName.replace('@iceworks/', '');
             if (packageFiles.includes(packageName)) {
-              console.log('extension', extensionFile, 'link package:', packageName);
+              console.log('extension: ', extensionFile, 'link package:', packageName);
               spawn.sync('npm', ['link', path.join(cwd, '../../', 'packages', packageName)], {
                 stdio: 'inherit',
                 cwd,
+              });
+            }
+          })
+        }
+      }
+
+      const webviewPath = path.join(cwd, 'web');
+      const webviewStat = await fse.stat(webviewPath);
+      if (fse.existsSync(webviewPath) && webviewStat.isDirectory()) {
+        const packageJSON: any = await fse.readJson(path.join(webviewPath, 'package.json'));
+        const packageNames = Reflect.ownKeys(packageJSON.dependencies).filter((moduleName: string) => {
+          return /^@iceworks/.test(moduleName);
+        });
+        // link packages to extension webview
+        if (packageNames.length) {
+          packageNames.forEach((packageName: string) => {
+            packageName = packageName.replace('@iceworks/', '');
+            if (packageFiles.includes(packageName)) {
+              console.log('extension web:', extensionFile, 'link package:', packageName);
+              spawn.sync('npm', ['link', path.join(webviewPath, '../../../', 'packages', packageName)], {
+                stdio: 'inherit',
+                cwd: webviewPath,
               });
             }
           })
