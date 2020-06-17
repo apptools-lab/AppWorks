5612a11a5a12444d6097f6f330a5727eba8981e0
luhc228 <luhengchang228@126.com>
2020-06-17 19:45:17 +0800

    feat: add collapse scaffold

--- extensions/iceworks-project-creator/web/src/pages/CreateProject/components/ScaffoldMarket/index.tsx
+++ extensions/iceworks-project-creator/web/src/pages/CreateProject/components/ScaffoldMarket/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Collapse } from '@alifd/next';
 import SelectCard from '@/components/SelectCard';
 import callService from '@/callService';
 import { IMaterialSource, IMaterialScaffold } from '@/iceworks/material-utils';
@@ -8,14 +9,17 @@ const ScaffoldMarket = ({ onScaffoldSelect, children }) => {
   const [materialSourceSelected, setMaterialSourceSelected] = useState<IMaterialSource>({});
   const [materialSelected, setMaterialSelected] = useState(null);
   const [materialSources, setMaterialSources] = useState<Array<IMaterialSource>>([]);
-  const [scaffoldMaterials, setScaffoldMaterials] = useState<IMaterialScaffold[]>([]);
+  const [mainScaffolds, setMainScaffolds] = useState<IMaterialScaffold[]>([]);
+  const [otherScaffolds, setOtherScaffolds] = useState<IMaterialScaffold[]>([]);
 
   async function onMaterialSourceClick(scaffold: IMaterialSource) {
     try {
       setMaterialSourceSelected(scaffold);
       const data = await getScaffolds(scaffold.source);
-      setScaffoldMaterials(data);
-      setMaterialSelected(null)
+      const { mainScaffolds, otherScaffolds } = data as any;
+      setMainScaffolds(mainScaffolds);
+      setOtherScaffolds(otherScaffolds);
+      setMaterialSelected(null);
     } catch (err) {
       // ignore
     }
@@ -35,7 +39,9 @@ const ScaffoldMarket = ({ onScaffoldSelect, children }) => {
     try {
       const scaffolds = await callService('project', 'getScaffolds', source) as IMaterialScaffold[];
       const whitelist = ['@alifd/fusion-design-pro', '@alifd/scaffold-lite', '@alifd/scaffold-simple', '@rax-materials/scaffolds-basic-app']
-      return scaffolds.filter(scaffold => whitelist.includes(scaffold.source.npm));
+      const main = scaffolds.filter(scaffold => whitelist.includes(scaffold.source.npm));
+      const other = scaffolds.filter(scaffold => !whitelist.includes(scaffold.source.npm));
+      return { mainScaffolds: main, otherScaffolds: other }
     } catch (e) {
       return [];
     }
@@ -50,12 +56,13 @@ const ScaffoldMarket = ({ onScaffoldSelect, children }) => {
         const source = materialSources[0].source;
 
         const data = await getScaffolds(source);
-        setScaffoldMaterials(data);
+        const { mainScaffolds, otherScaffolds } = data as any;
+        setMainScaffolds(mainScaffolds);
+        setOtherScaffolds(otherScaffolds);
       } catch (error) {
         // ignore
       }
     }
-
     initData();
   }, []);
   return (
@@ -73,8 +80,8 @@ const ScaffoldMarket = ({ onScaffoldSelect, children }) => {
             />
           ))}
         </div>
-        <div className={styles.materialScaffold}>
-          {scaffoldMaterials && scaffoldMaterials.map(item => (
+        <div className={styles.mainScaffolds}>
+          {mainScaffolds && mainScaffolds.map(item => (
             <SelectCard
               key={item.name}
               title={item.title}
@@ -85,6 +92,23 @@ const ScaffoldMarket = ({ onScaffoldSelect, children }) => {
               onClick={() => onScaffoldMaterialClick(item)}
             />
           ))}
+          <Collapse className={styles.collapse}>
+            <Collapse.Panel title="查看更多">
+              <div className={styles.otherScaffolds}>
+                {otherScaffolds && otherScaffolds.map(item => (
+                  <SelectCard
+                    key={item.name}
+                    title={item.title}
+                    content={<span className={styles.userSelect}>{item.description}</span>}
+                    media={<img height={120} src={item.screenshot} alt="screenshot" style={{ padding: '10px 10px 0' }} />}
+                    selected={materialSelected === item.name}
+                    style={{ width: 180, height: 250 }}
+                    onClick={() => onScaffoldMaterialClick(item)}
+                  />
+                ))}
+              </div>
+            </Collapse.Panel>
+          </Collapse>
         </div>
       </div>
       <div className={styles.action}>
