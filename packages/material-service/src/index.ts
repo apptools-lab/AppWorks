736d7c46e7574cf73eace70bafe4d374bdd07712
luhc228 <luhengchang228@126.com>
2020-06-19 12:22:46 +0800

    chore: add vue material not in aliInternal

--- packages/material-service/src/index.ts
+++ packages/material-service/src/index.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as kebabCase from 'lodash.kebabcase';
 import axios from 'axios';
+import { checkAliInternal } from 'ice-npm-utils';
 import { IMaterialSource, IMaterialData, IMaterialBase } from '@iceworks/material-utils';
 import { getProjectType } from '@iceworks/project-service';
 
@@ -10,18 +11,25 @@ const MATERIAL_BASE_REPOSITORY_URL = 'https://github.com/alibaba-fusion/next/tre
 const ICE_BASE_COMPONENTS_SOURCE = 'https://ice.alicdn.com/assets/base-components-1.x.json';
 const OFFICE_MATERIAL_SOURCES = [
   {
-    'name': 'PC Web',
-    'type': 'react',
-    'source': ICE_MATERIAL_SOURCE,
-    'description': '基于 Fusion 基础组件和 ice 脚手架的官方物料'
+    name: 'PC Web',
+    type: 'react',
+    source: ICE_MATERIAL_SOURCE,
+    description: '基于 Fusion 基础组件和 ice 脚手架的官方物料'
   },
   {
-    'name': '无线跨端',
-    'type': 'rax',
-    'source': 'https://ice.alicdn.com/assets/materials/rax-materials.json',
-    'description': '基于 Rax 组件和 Rax 脚手架的官方物料'
+    name: '无线跨端',
+    type: 'rax',
+    source: 'https://ice.alicdn.com/assets/materials/rax-materials.json',
+    description: '基于 Rax 组件和 Rax 脚手架的官方物料'
   }
 ]
+const VUE_MATERIAL_SOURCES = {
+  name: 'Vue 物料源',
+  type: 'vue',
+  source: 'https://ice.alicdn.com/assets/materials/vue-materials.json',
+  description: '基于 Element, Vue CLI 的 Vue 官方物料'
+}
+
 const dataCache: { [source: string]: IMaterialData } = {};
 
 const isIceMaterial = (source: string) => {
@@ -42,6 +50,9 @@ export const getOfficalMaterialSources = () => OFFICE_MATERIAL_SOURCES;
  */
 export function getSources(specifiedType?: string): IMaterialSource[] {
   const officalsources: IMaterialSource[] = getOfficalMaterialSources();
+  if (!checkAliInternal()) {
+    officalsources.push(VUE_MATERIAL_SOURCES);
+  }
   const userSources: IMaterialSource[] = vscode.workspace.getConfiguration('iceworks').get('materialSources', []);
   const sources = officalsources.concat(userSources);
   return specifiedType ? sources.filter(({ type }) => type === specifiedType) : sources;
