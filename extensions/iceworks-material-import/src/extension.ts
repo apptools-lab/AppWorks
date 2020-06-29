a7b1e7c8bd6f77fb825102185888826801c95a5d
luhc228 <luhengchang228@126.com>
2020-06-29 19:52:18 +0800

    feat: init project

--- extensions/iceworks-material-import/src/extension.ts
+++ extensions/iceworks-material-import/src/extension.ts
@@ -11,8 +11,7 @@ const { window, ViewColumn } = vscode;
 export function activate(context: vscode.ExtensionContext) {
   const { extensionPath, subscriptions, globalState } = context;
 
-  console.log('Congratulations, your extension "iceworks-page-builder" is now active!');
-
+  console.log('Congratulations, your extension "iceworks-material-import" is now active!');
   // data collection
   const logger = new Logger(name, globalState);
   logger.recordDAU();
@@ -23,21 +22,18 @@ export function activate(context: vscode.ExtensionContext) {
       version,
     }
   });
-
   // auto set configuration
   initExtensionConfiguration(globalState);
-
+  // init webview
   function activeWebview() {
-    const webviewPanel: vscode.WebviewPanel = window.createWebviewPanel('iceworks', '创建页面', ViewColumn.One, {
+    const webviewPanel: vscode.WebviewPanel = window.createWebviewPanel('iceworks', '导入物料', ViewColumn.One, {
       enableScripts: true,
       retainContextWhenHidden: true,
     });
     webviewPanel.webview.html = getHtmlForWebview(extensionPath);
     connectService(webviewPanel.webview, subscriptions, { services, logger });
   }
-  subscriptions.push(vscode.commands.registerCommand('iceworks-page-builder.create', function () {
+  subscriptions.push(vscode.commands.registerCommand('iceworks-material-import.start', function () {
     activeWebview();
   }));
 }
-
-export function deactivate() { }
