567da846b49d38a54d4015b4a1f9286c1058c666
yangfan <18767120422@163.com>
2020-07-02 10:41:56 +0800

    feat: add check

--- scripts/publish-extension.ts
+++ scripts/publish-extension.ts
@@ -1,13 +1,48 @@
 /**
  * Scripts to check unpublished version and run publish
  */
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import axios from 'axios';
 import { spawnSync } from 'child_process';
 import { IExtensionInfo, getExtensionInfos } from './getExtensionInfos';
 import extensionDepsInstall from './fn/extension-deps-install';
 
-// Wait for npm module publish and sync.
-function sleep(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+// Check published packages can be installed.
+function checkPackagePublished() {
+  const publishedPackages: string[] = JSON.parse(fs.readFileSync(path.join(__dirname, 'publishedPackages.temp.json'), 'utf-8'));
+
+  const timeout = 1000;
+  const maxDetectTimes = 3; 
+  return Promise.all(publishedPackages.map((publishedPackage) => {
+    return new Promise((resolve, retject) => {
+      
+      const info = publishedPackage.split(':');
+      // Example: @iceworks/project-service:0.1.8:lib/index.js
+      const name = info[0];
+      const version = info[1];
+      const mainFile = info[2];
+
+      let times = 0;
+      const timer = setInterval(() => {
+        if (times++ > maxDetectTimes) {
+          // Exit if detect times over maxDetectTimes.
+          clearInterval(timer);
+          retject(new Error(`${name}@${version} publish failed! Please try again.`));
+        } else {
+          axios(
+            `https://unpkg.com/${name}@${version}/${mainFile}`
+          ).then(() => {
+            // Can be installed.
+            clearInterval(timer);
+            resolve();
+          }).catch(() => {
+            // ignore
+          })
+        }
+      }, timeout);
+    })
+  }));
 }
 
 function publish(extension: string, directory: string, version: string): void {
@@ -23,9 +58,7 @@ function publish(extension: string, directory: string, version: string): void {
   });
 }
 
-// Wait 10s for npm
-console.log('Wait for npm publish');
-sleep(10000).then(() => {
+checkPackagePublished().then(() => {
   // Entry
   console.log('[PUBLISH] Start:');
   getExtensionInfos().then((extensionInfos: IExtensionInfo[]) => {
@@ -47,4 +80,6 @@ sleep(10000).then(() => {
     console.log(`[PUBLISH EXTENSION PRODUCTION] Complete (count=${publishedCount}):`);
     console.log(`${publishedExtensions.join('\n')}`);
   });
+}).catch((e) => {
+  console.error(e);
 });
--- scripts/publish-package.ts
+++ scripts/publish-package.ts
@@ -1,6 +1,8 @@
 /**
  * Scripts to check unpublished version and run publish
  */
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import { spawnSync } from 'child_process';
 import { IPackageInfo, getPackageInfos } from './getPackageInfos';
 
@@ -23,14 +25,16 @@ getPackageInfos().then((packageInfos: IPackageInfo[]) => {
   let publishedCount = 0;
   const publishedPackages = [];
   for (let i = 0; i < packageInfos.length; i++) {
-    const { name, directory, localVersion, shouldPublish } = packageInfos[i];
+    const { name, directory, localVersion, shouldPublish, mainFile } = packageInfos[i];
     if (shouldPublish) {
       publishedCount++;
       console.log(`--- ${name}@${localVersion} ---`);
       publish(name, localVersion, directory);
-      publishedPackages.push(`${name}:${localVersion}`);
+      publishedPackages.push(`${name}:${localVersion}:${mainFile}`);
     }
   }
   console.log(`[PUBLISH PACKAGE PRODUCTION] Complete (count=${publishedCount}):`)
   console.log(`${publishedPackages.join('\n')}`);
+  // Write temp file
+  fs.writeFileSync(path.join(__dirname, 'publishedPackages.temp.json'), JSON.stringify(publishedPackages));
 });
