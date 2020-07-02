43acf2feb1201f8c8969911c2718d0279d460795
alvinhui <alvin.hui@qq.com>
2020-07-02 23:57:16 +0800

    refactor: record activate

--- packages/common-service/src/log.ts
+++ packages/common-service/src/log.ts
@@ -9,7 +9,6 @@ const logCode = isElectron ? 'pack_app' : 'pack_web';
 const outside = '_outside';
 let isAlibaba: boolean;
 
-
 interface ILogParam {
   module: string;
   action: string;
@@ -94,6 +93,15 @@ export function recordDAU(storage: IStorage) {
   }, storage);
 }
 
+export function recordActivate(storage: IStorage, data: { extension: string; version?: string; }) {
+  return recordOnce({
+    namespace: MAIN_KEY,
+    module: LOGGER_MODULE_KEY,
+    action: 'activate',
+    data,
+  }, storage);
+}
+
 interface IStorage {
   get: (key: string) => any;
   update: (key: string, value: any) => void;
@@ -163,17 +171,21 @@ export class Logger {
    * @param version extension's version
    */
   public recordActivate(version?: string) {
-    return recordOnce(
-      {
-        namespace: MAIN_KEY,
-        module: LOGGER_MODULE_KEY,
-        action: 'activate',
-        data: {
-          extension: this.namespace,
-          version,
-        },
+    // uv
+    if (this.storage) {
+      recordActivate(this.storage, {
+        extension: this.namespace,
+        version,
+      });
+    }
+
+    // pv
+    this.record({
+      module: 'main',
+      action: 'active',
+      data: {
+        version,
       },
-      this.storage
-    );
+    });
   }
 }
