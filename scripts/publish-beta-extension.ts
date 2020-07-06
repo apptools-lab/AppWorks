88495a3c752ca0a025f87c9f53e30f8c854077d1
yangfan <18767120422@163.com>
2020-07-06 11:06:55 +0800

    feat: upload packed extension to OSS (#167)
    * feat: add upload
    
    * fix: review

--- scripts/publish-beta-extension.ts
+++ scripts/publish-beta-extension.ts
@@ -1,24 +1,17 @@
 /**
  * Scripts to check unpublished version and run beta publish
  */
-import * as oss from 'ali-oss';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import { spawnSync } from 'child_process';
+import uploadExtesions from './upload-extensions';
+import { getPublishedPackages } from './published-info';
 import { IExtensionInfo, getExtensionInfos } from './getExtensionInfos';
 import extensionDepsInstall from './fn/extension-deps-install';
 
-const ossClient = oss({
-  bucket: 'iceworks',
-  endpoint: 'oss-cn-hangzhou.aliyuncs.com',
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  accessKeySecret: process.env.ACCESS_KEY_SECRET,
-  timeout: '120s',
-});
-
 function updateBetaDependencies(extension: string, directory: string) {
   try {
-    const publishedPackages: string[] = JSON.parse(fs.readFileSync(path.join(__dirname, 'publishedPackages.temp.json'), 'utf-8'));
+    const publishedPackages: string[] = getPublishedPackages();
 
     if (fs.existsSync(directory)) {
       const packageFile = path.join(directory, 'package.json');
@@ -51,24 +44,12 @@ function publish(extension: string, directory: string, version: string): void {
     stdio: 'inherit',
     cwd: directory,
   });
-
-  // Upload to oss
-  const extensionFile = `${extension}-${version}.vsix`;
-  const extensionFilePath = path.resolve(directory, extensionFile);
-  ossClient
-    .put(`vscode-extensions/beta/${extensionFile}`, extensionFilePath)
-    .then(() => {
-      console.log(`[PUBLISH BETA] ${extensionFile} upload success.`);
-    })
-    .catch(() => {
-      console.log(`[ERROR] ${extensionFile} upload failed.`);
-    });
 }
 
 // Entry
 console.log('[PUBLISH BETA] Start:');
 getExtensionInfos().then((extensionInfos: IExtensionInfo[]) => {
-  const shouldPublishPackages: IExtensionInfo[] = [];
+  const shouldPublishExtensions: IExtensionInfo[] = [];
 
   for (let i = 0; i < extensionInfos.length; i++) {
     const { name, directory, shouldPublish } = extensionInfos[i];
@@ -77,8 +58,8 @@ getExtensionInfos().then((extensionInfos: IExtensionInfo[]) => {
       updateBetaDependencies(name, directory);
       // Update inside web project package json
       updateBetaDependencies(name, path.join(directory, 'web'));
-      
-      shouldPublishPackages.push(extensionInfos[i]);
+
+      shouldPublishExtensions.push(extensionInfos[i]);
     }
   }
 
@@ -88,14 +69,15 @@ getExtensionInfos().then((extensionInfos: IExtensionInfo[]) => {
   // Publish
   let publishedCount = 0;
   const publishedExtensions = [];
-  for (let i = 0; i < shouldPublishPackages.length; i++) {
-    const { name, directory, localVersion } = shouldPublishPackages[i];
+  for (let i = 0; i < shouldPublishExtensions.length; i++) {
+    const { name, directory, localVersion } = shouldPublishExtensions[i];
     publishedCount++;
     console.log(`--- ${name}@${localVersion} ---`);
 
     publish(name, directory, localVersion);
     publishedExtensions.push(`${name}:${localVersion}`);
   }
+  uploadExtesions(publishedExtensions);
   console.log(`[PUBLISH EXTENSION BETA] Complete (count=${publishedCount}):`);
   console.log(`${publishedExtensions.join('\n')}`);
 });
