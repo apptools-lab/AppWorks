2e77a400223c10f849de922c85f48ba3b71e0af2
luhc228 <luhengchang228@126.com>
2020-07-07 15:49:42 +0800

    style: material-ui

--- extensions/iceworks-material-import/src/extension.ts
+++ extensions/iceworks-material-import/src/extension.ts
@@ -20,9 +20,10 @@ export function activate(context: vscode.ExtensionContext) {
   initExtension(context);
   // init webview
   const columnToShowIn = vscode.window.activeTextEditor
-    ? ViewColumn.Beside
+    ? ViewColumn.Two
     : ViewColumn.One;
-
+  const layout = { orientation: 0, groups: [{ size: 0.8 }, { size: 0.2 }] };
+  vscode.commands.executeCommand('vscode.setEditorLayout', layout);
   function activeWebview() {
     const webviewPanel = window.createWebviewPanel('Iceworks', '使用物料 - Iceworks', { viewColumn: columnToShowIn, preserveFocus: true }, {
       enableScripts: true,
@@ -30,6 +31,12 @@ export function activate(context: vscode.ExtensionContext) {
       enableFindWidget: true,
     });
     webviewPanel.webview.html = getHtmlForWebview(extensionPath);
+    webviewPanel.onDidChangeViewState((e: vscode.WebviewPanelOnDidChangeViewStateEvent) => {
+
+    })
+    webviewPanel.onDidDispose(() => {
+      vscode.window.showInformationMessage('1234567890')
+    })
     connectService(webviewPanel, context, { services, logger });
   }
   subscriptions.push(vscode.commands.registerCommand('iceworks-material-import.start', function () {
