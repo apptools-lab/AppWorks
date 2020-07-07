9aeaceabec82b66d14be182c008f188af61cff49
luhc228 <luhengchang228@126.com>
2020-07-07 19:51:50 +0800

    fix: remove sylvanas

--- packages/block-service/src/index.ts
+++ packages/block-service/src/index.ts
@@ -3,7 +3,6 @@ import * as vscode from 'vscode';
 import * as fsExtra from 'fs-extra';
 import * as glob from 'glob';
 import * as readFiles from 'fs-readdir-recursive';
-import * as transfromTsToJs from 'sylvanas';
 import { getAndExtractTarball, readPackageJSON } from 'ice-npm-utils';
 import { getTarballURLByMaterielSource, IMaterialBlock } from '@iceworks/material-utils';
 import {
@@ -83,21 +82,6 @@ export const bulkDownload = async function (blocks: IMaterialBlock[], localPath:
       const projectType = await getProjectLanguageType();
 
       console.log('blockType: ', blockType, 'projectType: ', projectType);
-
-      // transfrom ts to js
-      if (blockType === 'ts' && projectType === 'js') {
-        const files = glob.sync('**/*.@(ts|tsx)', {
-          cwd: blockSourceSrcPath,
-        });
-
-        console.log('transfrom ts to js', files.join(','));
-
-        transfromTsToJs(files, {
-          cwd: blockSourceSrcPath,
-          outDir: blockSourceSrcPath,
-          action: 'overwrite',
-        });
-      }
 
       await fsExtra.move(blockSourceSrcPath, blockDir);
       await fsExtra.remove(blockTempDir);
