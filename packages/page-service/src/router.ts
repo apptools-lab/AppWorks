b74cff48797e0d86fcafd47105e8fbc6b55dcd0f
luhc228 <luhengchang228@126.com>
2020-07-15 12:28:28 +0800

    feat: add traverse code

---
+++ packages/page-service/src/router.ts
@@ -0,0 +1,321 @@
+import * as path from 'path';
+import * as fse from 'fs-extra';
+import traverse from '@babel/traverse';
+import * as parser from '@babel/parser';
+import * as t from '@babel/types';
+import { IRouter, IRouterOptions } from './types';
+import generate from '@babel/generator';
+import formatCodeFromAST from './formatCodeFromAST';
+
+const routerConfigFileName = 'routes';
+const ROUTER_CONFIG_VARIABLE = 'routerConfig';
+const ROUTE_PROP_WHITELIST = ['component', 'path', 'exact', 'strict', 'sensitive', 'children', 'redirect'];
+const LAYOUT_DIRECTORY = 'layouts';
+const PAGE_DIRECTORY = 'pages';
+
+export async function getRouterConfigAST(projectPath: string) {
+  const routeConfigPath = path.join(projectPath, routerConfigFileName);
+  const routerConfigString = await fse.readFile(routeConfigPath, 'utf-8');
+  const routerConfigAST = getASTByCode(routerConfigString);
+  return routerConfigAST;
+}
+
+function getASTByCode(code: string) {
+  return parser.parse(code, {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    plugins: [
+      'dynamicImport'
+    ]
+  })
+}
+
+function parseRoute(elements) {
+  const config = [];
+  elements.forEach(element => {
+    const { properties } = element;
+    const item: any = {};
+    properties.forEach((property) => {
+      const { key, value } = property;
+      const { name: keyName } = key;
+
+      // component is react Component
+      if (keyName === 'component') {
+        item[keyName] = value.name;
+      } else if (keyName === 'children') {
+        // children is array
+        item.children = parseRoute(value.elements);
+      } else if (ROUTE_PROP_WHITELIST.indexOf(keyName) > -1) {
+        item[keyName] = value.value;
+      }
+    });
+    if (Object.keys(item).length > 0) {
+      config.push(item);
+    }
+  });
+}
+export async function getAll(routerConfigAST) {
+  let config = [];
+
+  traverse(routerConfigAST, {
+    VariableDeclarator: ({ node }) => {
+      if (
+        t.isIdentifier(node.id, { name: ROUTER_CONFIG_VARIABLE })
+        && t.isArrayExpression(node.init)
+      ) {
+        config = parseRoute(node.init.elements);
+      }
+    },
+  });
+
+  return config;
+}
+
+export async function bulkCreate(projectPath: string, data: IRouter[], options: IRouterOptions = {}): Promise<void> {
+  const { replacement = false, parent } = options;
+  const routerConfigAST = getRouterConfigAST(projectPath);
+  const routeConfigPath = path.join(projectPath, routerConfigFileName);
+  const currentData = await getAll(projectPath);
+
+  if (!replacement) {
+    if (parent) {
+      const parentRouter = currentData.find((item) => {
+        if (item.children && item.path === parent) {
+          return true;
+        }
+        return false;
+      });
+      if (parentRouter) {
+        parentRouter.children = parentRouter.children.concat(data);
+        data = currentData;
+      }
+    } else {
+      data = currentData.concat(data);
+    }
+  }
+  setData(data, routerConfigAST, routeConfigPath);
+}
+
+function setData(data, routerConfigAST, routeConfigPath) {
+  const dataAST = getASTByCode(JSON.stringify(sortData(data)));
+  const arrayAST = dataAST.program.body[0];
+
+  changeImportDeclarations(routerConfigAST, data);
+  /**
+   * { path: '/a', component: 'Page' }
+   *          transform to
+   * { path: '/a', component: Page }
+   */
+  traverse(dataAST, {
+    ObjectProperty({ node }) {
+      if (['component'].indexOf(node.key.value) > -1) {
+        node.value = t.identifier(node.value.value);
+      }
+    },
+  });
+  traverse(routerConfigAST, {
+    VariableDeclarator({ node }) {
+      if (
+        t.isIdentifier(node.id, { name: ROUTER_CONFIG_VARIABLE })
+        && t.isArrayExpression(node.init)
+      ) {
+        node.init = arrayAST;
+      }
+    },
+  });
+  fse.writeFileSync(
+    routeConfigPath,
+    formatCodeFromAST(routerConfigAST)
+  );
+}
+
+export function sortData(data: any) {
+
+}
+
+/**
+ * 1. constant if there is layout or component in the data and ImportDeclarations
+ * 2. remove import if there is no layout or component in the data
+ * 3. add import if there is no layout or component in the ImportDeclarations
+ */
+function changeImportDeclarations(routerConfigAST, data) {
+  const importDeclarations = [];
+  const removeIndex = [];
+  // router import page or layout have @
+  let existAtSign = false;
+  this.existLazy = false;
+
+  traverse(routerConfigAST, {
+    ImportDeclaration: ({ node, key }) => {
+      const { source } = node;
+      // parse import declaration to get directory type (layouts or pages)
+      // support three path types
+      // 1. import xxx from 'pages/xxx';
+      // 2. import xxx from './pages/xxx';
+      // 3. import xxx from '@/pages/xxx';
+      const noPrefixReg = /^(layouts|pages)\//;
+      const hasPrefixReg = /^(\.|@)\/(layouts|pages)\//;
+      const reg = this.noPathPrefix ? noPrefixReg : hasPrefixReg;
+      const idx = this.noPathPrefix ? 1 : 2;
+      const match = source.value.match(reg);
+
+      if (match && match[idx]) {
+        const { specifiers } = node;
+        const { name } = specifiers[0].local;
+        if (!this.noPathPrefix) {
+          existAtSign = match[idx - 1] === '@';
+        }
+        importDeclarations.push({
+          index: key,
+          name,
+          type: match[idx],
+        });
+      }
+    },
+
+    // parse eg. `const Forbidden = React.lazy(() => import('./pages/Exception/Forbidden'));`
+    VariableDeclaration: ({ node, key }) => {
+      const code = generate(node.declarations[0]).code;
+      // parse const declaration to get directory type (layouts or pages)
+      // support three path types
+      // 1. const xxx = React.lazy(() => import('pages/xxx'));
+      // 2. const xxx = React.lazy(() => import('./pages/xxx'));
+      // 3. const xxx = React.lazy(() => import('@/pages/xxx'));
+      const noPrefixReg = /(\w+)\s=\sReact\.lazy(.+)import\(['|"]((\w+)\/.+)['|"]\)/;
+      const hasPrefixReg = /(\w+)\s=\sReact\.lazy(.+)import\(['|"]((\.|@)\/(\w+)\/.+)['|"]\)/;
+      const matchLazyReg = this.noPathPrefix ? noPrefixReg : hasPrefixReg;
+      const idx = this.noPathPrefix ? 4 : 5;
+      const match = code.match(matchLazyReg);
+
+      if (match && match.length > idx) {
+        this.existLazy = true;
+        existAtSign = match[idx - 1] === '@';
+        importDeclarations.push({
+          index: key,
+          name: match[1],
+          type: match[idx],
+        });
+      }
+    },
+  });
+
+  /**
+   * remove import if there is no layout or component in the data
+   */
+  importDeclarations.forEach((importItem) => {
+    const { name, type, index } = importItem;
+    let needRemove = false;
+
+    // match layout or page
+    if (type) {
+      let findRouter = null;
+
+      if (type === LAYOUT_DIRECTORY) {
+        // layout only first layer
+        findRouter = data.find(item => item.children && item.component === name);
+      } else if (type === PAGE_DIRECTORY) {
+        findRouter = data.find(item => {
+          let pageItem = null;
+
+          if (!item.children && item.component === name) {
+            pageItem = item;
+          }
+
+          if (item.children) {
+            item.children.forEach((route) => {
+              if (route.component === name) {
+                pageItem = route;
+              }
+            });
+          }
+
+          return pageItem;
+        });
+      }
+      if (!findRouter) {
+        needRemove = true;
+      }
+    }
+
+    if (needRemove) {
+      removeIndex.unshift(index);
+    }
+  });
+
+  removeIndex.forEach((index) => {
+    routerConfigAST.program.body.splice(index, 1);
+  });
+
+  // add new page or layout
+  function setNewComponent(type, component) {
+    const componentExist = existImport(importDeclarations, component, type);
+
+    // no component dont add import
+    if (!component) {
+      return false;
+    }
+
+    if (!componentExist && !newImports.find(item => item.name === component)) {
+      newImports.push({
+        type,
+        name: component,
+      });
+    }
+  }
+
+  /**
+   * add import if there is no layout or component in the ImportDeclarations
+   */
+  const newImports = [];
+  data.forEach(({ component, children }) => {
+    if (children) {
+      setNewComponent(LAYOUT_DIRECTORY, component);
+      children.forEach((route) => setNewComponent(PAGE_DIRECTORY, route.component));
+    } else {
+      setNewComponent(PAGE_DIRECTORY, component);
+    }
+  });
+
+  /**
+   * add import to ast
+   *  eg.
+   *     import Page1 from './pages/Page1';
+   *            or
+   *     const Profile = React.lazy(() => import('./pages/Profile'));
+   */
+  let lazyCode = '';
+  let importCode = '';
+  const sign = '@';
+  newImports.forEach(({ name, type }) => {
+    if (this.noPathPrefix) {
+      importCode += `import ${name} from '${type}/${name}';\n`;
+    } else if (!this.existLazy || type === LAYOUT_DIRECTORY) {
+      // layour or not exist lazy use `import Page from '@/pages/Page'`
+      importCode += `import ${name} from '${sign}/${type}/${name}';\n`;
+    } else {
+      // use lazy `const Page = React.lazy(() => import('@/pages/Page'))`
+      lazyCode += `const ${name} = React.lazy(() => import('${sign}/${type}/${name}'));\n`;
+    }
+  });
+
+  // get ast from lazy or import code
+  const lazyCodeAST = this.getASTByCode(lazyCode);
+  const importCodeAST = this.getASTByCode(importCode);
+
+  const lastIndex = this.findLastImportIndex(routerConfigAST);
+  routerConfigAST.program.body.splice(lastIndex, 0, ...lazyCodeAST.program.body);
+  routerConfigAST.program.body.splice(
+    this.existLazy ? lastIndex - 1 : lastIndex,
+    0,
+    ...importCodeAST.program.body
+  );
+}
+
+function existImport(list, name, type) {
+  return list.some((item) => {
+    if (name === item.name && type === item.type) {
+      return true;
+    }
+    return false;
+  });
+}
---
+++ packages/page-service/src/types.ts
@@ -0,0 +1,27 @@
+export interface IRouter {
+  /**
+ * URL path
+ */
+  path: string;
+
+  /**
+   * component name
+   */
+  component?: string;
+
+  /**
+   * layout name
+   */
+  layout?: string;
+
+  /**
+   * children routes
+   */
+  children?: IRouter[];
+}
+
+export interface IRouterOptions {
+  type?: string;
+  replacement?: boolean;
+  parent?: string;
+}
