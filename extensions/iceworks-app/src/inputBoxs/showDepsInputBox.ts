bc7c778c7c62b0d0c51e99bcefa42d077c5cce2a
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-07-18 10:38:50 +0800

    feat: i18n功能包及测试用例 「Quickpicks & showDepsInputBox」

--- extensions/iceworks-app/src/inputBoxs/showDepsInputBox.ts
+++ extensions/iceworks-app/src/inputBoxs/showDepsInputBox.ts
@@ -1,14 +1,15 @@
 import * as vscode from 'vscode';
 import { NodeDepTypes, ITerminalMap } from '../types';
 import executeCommand from '../commands/executeCommand';
+import {i18n} from '../i18n';
 
-export default async function showDepsInputBox(terminals: ITerminalMap, nodeDependenciesInstance: any, depType: NodeDepTypes) {
+export default async function showDepsInputBox(terminals: ITerminalMap, nodeDependenciesInstance: any, _depType: NodeDepTypes) {
   const result = await vscode.window.showInputBox({
-    placeHolder: '例如: lodash react@latest',
-    prompt: `请输入需要添加到 ${depType} 的依赖名称, 支持通过空格添加多个依赖`
+    placeHolder: i18n.format('extension.iceworksApp.showDepsInputBox.materialImport.placeHolder'),
+    prompt: i18n.format('extension.iceworksApp.showDepsInputBox.materialImport.prompt',{depType: _depType})
   });
   if (!result) {
     return;
   }
-  executeCommand(terminals, nodeDependenciesInstance.getAddDependencyScript(depType, result));
+  executeCommand(terminals, nodeDependenciesInstance.getAddDependencyScript(_depType, result));
 }
