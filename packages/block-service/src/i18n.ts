875421ae781d463857703e032e13f7196d872f98
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-07-20 17:28:46 +0800

    feat: 根据 I18n 依赖包的新输出方式进行调整

--- packages/block-service/src/i18n.ts
+++ packages/block-service/src/i18n.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
-import i18n from '@iceworks/i18n';
+import I18nService from '@iceworks/i18n';
 import * as zhCNTextMap from './locales/zh-CN.json';
 import * as enUSTextMap from './locales/en-US.json';
 
-export default i18n;
-
+const i18n = new I18nService();
 i18n.registry('zh-cn',zhCNTextMap);
 i18n.registry('en',enUSTextMap);
 i18n.setLocal(vscode.env.language);
+
+export default i18n;
