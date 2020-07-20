7fb82fcaf4c904fcbccaf7512d0e30ba49bde03c
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-07-20 22:00:24 +0800

    refactor: move export to bottom

--- extensions/iceworks-app/src/i18n.ts
+++ extensions/iceworks-app/src/i18n.ts
@@ -4,9 +4,10 @@ import * as zhCNTextMap from './locales/zh-CN.json';
 import * as enUSTextMap from './locales/en-US.json';
 
 const i18n = new I18nService();
-export default i18n;
 
 // set I18n
 i18n.registry('zh-cn',zhCNTextMap);
 i18n.registry('en',enUSTextMap);
 i18n.setLocal(vscode.env.language);
+
+export default i18n;
