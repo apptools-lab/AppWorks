91a8dabd60d5bff41eacb2899094b2fd847397e0
alvinhui <alvin.hui@qq.com>
2020-07-22 16:16:41 +0800

    fix: default locale

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -2,30 +2,24 @@ import * as _ from 'lodash';
 import { II18n, ITextMap } from './type';
 
 export default class I18nService implements II18n{
-  localesTextMap: { [locale: string]: ITextMap }= {};
+  localesTextMap: { [locale: string]: ITextMap } = {};
 
   currentTextMap: ITextMap = {};
-
-  locale = 'zh-cn';
 
   registry(locale: string,text: ITextMap){
     this.localesTextMap[locale] = text;
   }
 
-  // 设置当前使用的语言
   setLocal(locale: string){
-    this.currentTextMap = this.localesTextMap[locale];
+    this.currentTextMap = this.localesTextMap[locale] || this.localesTextMap[Object.keys(this.localesTextMap)[0]];
   }
   
-  // 根据文本标识符获取对应的文本
   format(contentKey: string, args?: object){
-    let i18nformatString = this.currentTextMap[contentKey];
+    const i18nformatString = this.currentTextMap[contentKey];
     if(!i18nformatString){
       return '';
     }
-    if(args){
-      i18nformatString = _.template(i18nformatString)(args);
-    }
-    return i18nformatString;
+    
+    return args ? _.template(i18nformatString)(args) : i18nformatString;
   }
 }
