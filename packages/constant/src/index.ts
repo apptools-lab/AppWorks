fb14e16dd0051c79c30b65be59aae326b4f78f7c
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-07-24 07:52:58 +0800

    fix: use namespace to make test work

--- packages/constant/src/index.ts
+++ packages/constant/src/index.ts
@@ -1,17 +1,17 @@
-import { decode } from 'js-base64'
+import { Base64 } from 'js-base64'
 
-export const ALI_NPM_REGISTRY = decode('aHR0cHM6Ly9yZWdpc3RyeS5ucG0uYWxpYmFiYS1pbmMuY29t');
-export const ALI_GITACCOUNT_URL  = decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vcHJvZmlsZS9hY2NvdW50');
-export const ALI_GITACCOUNT_SHORTURL = decode('Z2l0bGFiLmFsaWJhYmEtaW5jLmNvbS9wcm9maWxlL2FjY291bnQ=');
-export const ALI_GITLABGROUPS_API = decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vYXBpL3YzL2dyb3Vwcw==');
-export const ALI_GITLABPROJECTS_API = decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vYXBpL3YzL3Byb2plY3Rz');
-export const ALI_FUSION_MATERIAL_URL= decode('aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29tL2FwaS92MS9zaXRlcy8xMTk0L21hdGVyaWFscw==');
-export const ALI_YUEQU_URL = decode('aHR0cHM6Ly95dXF1ZS5hbGliYWJhLWluYy5jb20vaWNlL3JkeTk5cC9hbmd3eXg=');
-export const ALI_UNPKG_URL = decode('aHR0cHM6Ly91bnBrZy5hbGliYWJhLWluYy5jb20=');
-export const ALI_CHECKNODE_URL = decode('aHR0cHM6Ly9pY2UuYWxpYmFiYS1pbmMuY29tL2NoZWNrLm5vZGU=');
-export const ALI_FUSION_V1_MATERIAL = decode('aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29tL2FwaS92MS9zaXRlcy8yMS9tYXRlcmlhbHM=');
-export const ALI_CREATETASK_URL = decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvZ2VuZXJhdG9yL2dlbmVyYXRvci9jcmVhdGV0YXNr');
-export const ALI_TASKRESULT_URL = decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvZ2VuZXJhdG9yL2dlbmVyYXRvci90YXNr');
-export const ALI_APPLYREPO_URL = decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvd29yay9yZXBvL2FwcGx5');
-export const ALI_GITLAB_URL = decode('Z2l0QGdpdGxhYi5hbGliYWJhLWluYy5jb20=');
-export const ALI_EMAIL= decode('YWxpYmFiYS1pbmMuY29t');
+export const ALI_NPM_REGISTRY = Base64.decode('aHR0cHM6Ly9yZWdpc3RyeS5ucG0uYWxpYmFiYS1pbmMuY29t');
+export const ALI_GITACCOUNT_URL  = Base64.decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vcHJvZmlsZS9hY2NvdW50');
+export const ALI_GITACCOUNT_SHORTURL = Base64.decode('Z2l0bGFiLmFsaWJhYmEtaW5jLmNvbS9wcm9maWxlL2FjY291bnQ=');
+export const ALI_GITLABGROUPS_API = Base64.decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vYXBpL3YzL2dyb3Vwcw==');
+export const ALI_GITLABPROJECTS_API = Base64.decode('aHR0cDovL2dpdGxhYi5hbGliYWJhLWluYy5jb20vYXBpL3YzL3Byb2plY3Rz');
+export const ALI_FUSION_MATERIAL_URL= Base64.decode('aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29tL2FwaS92MS9zaXRlcy8xMTk0L21hdGVyaWFscw==');
+export const ALI_YUEQU_URL = Base64.decode('aHR0cHM6Ly95dXF1ZS5hbGliYWJhLWluYy5jb20vaWNlL3JkeTk5cC9hbmd3eXg=');
+export const ALI_UNPKG_URL = Base64.decode('aHR0cHM6Ly91bnBrZy5hbGliYWJhLWluYy5jb20=');
+export const ALI_CHECKNODE_URL = Base64.decode('aHR0cHM6Ly9pY2UuYWxpYmFiYS1pbmMuY29tL2NoZWNrLm5vZGU=');
+export const ALI_FUSION_V1_MATERIAL = Base64.decode('aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29tL2FwaS92MS9zaXRlcy8yMS9tYXRlcmlhbHM=');
+export const ALI_CREATETASK_URL = Base64.decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvZ2VuZXJhdG9yL2dlbmVyYXRvci9jcmVhdGV0YXNr');
+export const ALI_TASKRESULT_URL = Base64.decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvZ2VuZXJhdG9yL2dlbmVyYXRvci90YXNr');
+export const ALI_APPLYREPO_URL = Base64.decode('aHR0cHM6Ly9hcGkuZGVmLmFsaWJhYmEtaW5jLmNvbS9hcGkvd29yay9yZXBvL2FwcGx5');
+export const ALI_GITLAB_URL = Base64.decode('Z2l0QGdpdGxhYi5hbGliYWJhLWluYy5jb20=');
+export const ALI_EMAIL= Base64.decode('YWxpYmFiYS1pbmMuY29t');
