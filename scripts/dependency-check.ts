43cf0c53a59a92dff60e17bd69f4bacacf16106e
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-07-23 21:51:56 +0800

    fix: add '--ignore-module' to dependency-check

--- scripts/dependency-check.ts
+++ scripts/dependency-check.ts
@@ -5,7 +5,7 @@ import getPackages from './fn/getPackages';
 (async () => {
   const { packageDirs } = await getPackages();
   packageDirs.forEach((pkgDir) => {
-    execa.commandSync(`dependency-check ${pkgDir} --missing -i vscode -i puppeteer -i @babel/runtime`, {
+    execa.commandSync(`dependency-check ${pkgDir} --missing -i --ignore-module vscode -i puppeteer -i @babel/runtime`, {
       cwd: pkgDir,
       stdio: 'inherit'
     });
