ce89bdac1f1264af2931c692e5e937395798e175
yangfan <18767120422@163.com>
2020-08-06 15:03:42 +0800

    feat: update

--- packages/eslint-plugin-security-practices/src/configs/recommended.ts
+++ packages/eslint-plugin-security-practices/src/configs/recommended.ts
@@ -5,7 +5,6 @@ module.exports = deepmerge(eslint, {
   plugins: ['@iceworks/security-practices'],
   rules: {
     'react/react-in-jsx-scope': 'off', // For rax
-    '@iceworks/security-practices/no-bsd-licenses': 'warn',
     '@iceworks/security-practices/no-http-url': 'warn',
     '@iceworks/security-practices/no-internal-url': ['warn', ['taobao.net']],
     '@iceworks/security-practices/no-secret-info': 'error',
