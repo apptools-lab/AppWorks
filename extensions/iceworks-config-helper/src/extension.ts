d0be96e45cfd30981b097a023c867e90beab687c
许文涛 <alvin.hui@qq.com>
2020-08-13 17:18:11 +0800

    fix: config helper (#425)
    * fix: command language && paltette
    
    * fix: show source
    
    * feat: make sure uri is validate
    
    * style: fix by prettier

--- extensions/iceworks-config-helper/src/extension.ts
+++ extensions/iceworks-config-helper/src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { connectService, getHtmlForWebview } from '@iceworks/vscode-webview/lib/vscode';
-import { initExtension } from '@iceworks/common-service';
-import { recordDAU, Recorder } from '@iceworks/recorder';
+import { initExtension, registerCommand } from '@iceworks/common-service';
+import { Recorder } from '@iceworks/recorder';
 import { getProjectFramework } from '@iceworks/project-service';
 import { services, getIsUpdatingJsonFile } from './services';
 import i18n from './i18n';
@@ -11,12 +11,17 @@ import {
   setEditingJsonFileUri,
   getEditingJsonFileUri,
   getBaseNameFormUri,
+  getExtNameFormUri,
   getEditingJsonEditor,
 } from './utils';
 
 // eslint-disable-next-line
 const { name, version } = require('../package.json');
 const recorder = new Recorder(name, version);
+
+function setPanelActiveContext(value) {
+  vscode.commands.executeCommand('setContext', 'iceworks.config-helper.panelAcitve', value);
+}
 
 export async function activate(context: vscode.ExtensionContext) {
   await setJsonValidationUrl();
@@ -28,7 +33,6 @@ export async function activate(context: vscode.ExtensionContext) {
 
   let configWebviewPanel: vscode.WebviewPanel | undefined;
 
-  // TODO: 是否需要将这个函数从 active 中剥离出来？
   function activeConfigWebview(jsonFileUri: vscode.Uri) {
     if (!canEditInPanel(jsonFileUri)) {
       vscode.window.showWarningMessage(
@@ -38,10 +42,16 @@ export async function activate(context: vscode.ExtensionContext) {
       );
     } else {
       setEditingJsonFileUri(jsonFileUri);
+      recorder.record({
+        module: 'main',
+        action: 'activeConfigWebview',
+      });
+
       if (configWebviewPanel) {
         configWebviewPanel.dispose();
       }
 
+      setPanelActiveContext(true);
       configWebviewPanel = vscode.window.createWebviewPanel(
         'iceworks',
         i18n.format('extension.iceworksConfigHelper.index.webviewTitle'),
@@ -55,34 +65,65 @@ export async function activate(context: vscode.ExtensionContext) {
       configWebviewPanel.onDidDispose(
         () => {
           configWebviewPanel = undefined;
-          vscode.commands.executeCommand('setContext', 'iceworks.config-helper.panelAcitve', false);
+          setPanelActiveContext(false);
         },
         null,
         context.subscriptions
       );
-      connectService(configWebviewPanel, context, { services, recorder });
-
-      vscode.commands.executeCommand('setContext', 'iceworks.config-helper.panelAcitve', true);
-      recordDAU();
-      recorder.record({
-        module: 'main',
-        action: 'activeConfigWebview',
+      configWebviewPanel.onDidChangeViewState((webviewPanelOnDidChangeViewStateEvent) => {
+        setPanelActiveContext(webviewPanelOnDidChangeViewStateEvent.webviewPanel.active);
       });
+      connectService(configWebviewPanel, context, { services, recorder });
     }
   }
 
   subscriptions.push(
-    vscode.commands.registerCommand('iceworks-config-helper.configPanel.start', (uri) => {
-      activeConfigWebview(uri);
-    }),
-    vscode.commands.registerCommand('iceworks-config-helper.configPanel.showSource', () => {
-      const sourceFilePanel = getEditingJsonEditor();
-      if (sourceFilePanel) {
-        sourceFilePanel.show();
+    registerCommand('iceworks-config-helper.configPanel.start', (uri?) => {
+      uri = uri || vscode.window.activeTextEditor?.document.uri;
+      if (uri) {
+        activeConfigWebview(uri);
       } else {
-        vscode.window.showTextDocument(getEditingJsonFileUri(), {
-          viewColumn: 2,
-        });
+        vscode.window.showWarningMessage(i18n.format('extension.iceworksConfigHelper.start.error'));
+      }
+    }),
+    registerCommand('iceworks-config-helper.configPanel.showSource', async () => {
+      recorder.record({
+        module: 'main',
+        action: 'activeShowSource',
+      });
+
+      const sourceFilePanel = getEditingJsonEditor();
+
+      let textDocument: vscode.TextDocument;
+      if (sourceFilePanel) {
+        console.debug('sourceFilePanel', sourceFilePanel);
+        textDocument = sourceFilePanel.document;
+      } else {
+        const editingJsonFileUri = getEditingJsonFileUri();
+        console.debug('editingJsonFileUri', editingJsonFileUri);
+        if (editingJsonFileUri) {
+          try {
+            textDocument = await vscode.workspace.openTextDocument(editingJsonFileUri);
+          } catch (error) {
+            // ignore ...
+          }
+        }
+      }
+
+      // @ts-ignore
+      if (textDocument) {
+        try {
+          await vscode.window.showTextDocument(textDocument, {
+            viewColumn: 2,
+          });
+        } catch (error) {
+          console.error(error);
+          vscode.window.showWarningMessage(
+            i18n.format('extension.iceworksConfigHelper.showSource.error', {
+              uri: textDocument.uri.fsPath,
+            })
+          );
+        }
       }
     })
   );
