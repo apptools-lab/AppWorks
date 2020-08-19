de1e193666827cd8d592543185f61e9a54c6550f
luhc228 <luhengchang228@gmail.com>
2020-08-19 23:15:56 +0800

    feat: support quick entry in tree view

---
+++ extensions/iceworks-app/src/views/quickEntriesView.ts
@@ -0,0 +1,56 @@
+import * as vscode from 'vscode';
+import { entryOptions } from '../constants';
+
+export class QuickEntriesProvider implements vscode.TreeDataProvider<QuickEntryItem> {
+  private extensionContext: vscode.ExtensionContext;
+
+  constructor(context: vscode.ExtensionContext) {
+    this.extensionContext = context;
+  }
+
+  getTreeItem(element: QuickEntryItem): vscode.TreeItem {
+    return element;
+  }
+
+  async getChildren() {
+    const quickEntries = this.getEntries();
+    return Promise.resolve(quickEntries);
+  }
+
+  private getEntries() {
+    return entryOptions.map(option => {
+      const { label, detail, command } = option;
+      const entryCommand: vscode.Command = {
+        command,
+        title: label
+      };
+      return new QuickEntryItem(this.extensionContext, label, detail, entryCommand)
+    })
+  }
+}
+
+class QuickEntryItem extends vscode.TreeItem {
+  constructor(
+    public readonly extensionContext: vscode.ExtensionContext,
+    public readonly label: string,
+    public readonly tooltip: string,
+    public readonly command: vscode.Command,
+  ) {
+    super(label);
+  }
+
+  iconPath = {
+    dark: vscode.Uri.file(this.extensionContext.asAbsolutePath('assets/dark/entry.svg')),
+    light: vscode.Uri.file(this.extensionContext.asAbsolutePath('assets/light/entry.svg')),
+  };
+
+  contextValue = 'quickEntry';
+}
+
+
+export function createQuickEntriesTreeView(context: vscode.ExtensionContext) {
+  const quickEntriesProvider = new QuickEntriesProvider(context);
+  const treeView = vscode.window.createTreeView('quickEntries', { treeDataProvider: quickEntriesProvider });
+
+  return treeView;
+}
