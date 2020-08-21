5b0eade71c49cc0b731ce3af57ede1cfdf8a64e5
yangfan <18767120422@163.com>
2020-08-21 10:47:05 +0800

    feat: update

--- packages/eslint-plugin-best-practices/src/rules/deps-no-ice-scripts.ts
+++ packages/eslint-plugin-best-practices/src/rules/deps-no-ice-scripts.ts
@@ -18,8 +18,46 @@ module.exports = {
   },
 
   create(context) {
+    if (path.basename(context.getFilename()) !== 'package.json') {
+      return {};
+    }
+
     return {
-      // give me methods
+      Property: function handleRequires(node: any) {
+        if (
+          node.key &&
+          node.key.value &&
+          (node.key.value === 'dependencies' || node.key.value === 'devDependencies') &&
+          node.value &&
+          node.value.properties
+        ) {
+          node.value.properties.forEach((property) => {
+            if (property.key && property.key.value) {
+              const dependencyName = property.key.value;
+              console.log(dependencyName);
+
+              if (dependencyName === 'ice-scripts') {
+                console.log(1111);
+              }
+
+              // const dependencyPackage = fs.readJSONSync(dependencyPackageFile);
+              // const license = dependencyPackage.license || '';
+              // https://spdx.org/licenses/
+              //   if (license.indexOf('Patent') > -1) {
+              //     context.report({
+              //       loc: property.loc,
+              //       messageId: 'noPatentLicenses',
+              //       data: {
+              //         dependencyName,
+              //         license,
+              //       },
+              //     });
+
+              // }
+            }
+          });
+        }
+      },
     };
   },
 };
