9b350562fe33df4df3c20872dca5038b20723800
alvinhui <alvin.hui@qq.com>
2020-08-29 23:30:51 +0800

    chore: do not shouw informationMessage

--- extensions/iceworks-app/src/autoSetViewContext.ts
+++ extensions/iceworks-app/src/autoSetViewContext.ts
@@ -1,11 +1,9 @@
 import * as vscode from 'vscode';
 import { getProjectType } from '@iceworks/project-service';
-import i18n from './i18n';
 
 export default async function () {
   let isNotTargetProject;
   if (!vscode.workspace.rootPath) {
-    vscode.window.showInformationMessage(i18n.format('extension.iceworksApp.extension.emptyWorkplace'));
     isNotTargetProject = true;
   } else {
     try {
