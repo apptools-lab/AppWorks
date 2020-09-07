22596555781a32714053870fb4e24c7caa9a019e
yangfan <18767120422@163.com>
2020-09-07 14:29:59 +0800

    feat: update security practices

--- packages/eslint-plugin-security-practices/src/configs/recommended.ts
+++ packages/eslint-plugin-security-practices/src/configs/recommended.ts
@@ -26,5 +26,17 @@ module.exports = deepmerge(defaultEslintConfig, {
     '@iceworks/security-practices/no-internal-url': ['warn', ['taobao.net']],
     '@iceworks/security-practices/no-secret-info': 'error',
     '@iceworks/security-practices/no-sensitive-word': ['warn', ['fuck']],
+    // Ignore these rules when check security practices
+    camelcase: 0,
+    'dot-notation': 0,
+    indent: 0,
+    'no-empty': 0,
+    'no-var': 0,
+    'no-underscore-dangle': 0,
+    'object-shorthand': 0,
+    'prefer-template': 0,
+    quotes: 0,
+    'spaced-comment': 0,
+    'vars-on-top': 0,
   },
 });
