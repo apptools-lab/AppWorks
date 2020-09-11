d9fb85b9009e99cecd671dade1f364addcd25f12
yangfan <18767120422@163.com>
2020-09-11 11:56:59 +0800

    feat: update

--- packages/doctor/src/index.ts
+++ packages/doctor/src/index.ts
@@ -3,7 +3,7 @@ import { IDoctorOptions } from './types/Doctor';
 import { IScanOptions, IScannerReports } from './types/Scanner';
 
 // Ignore directories
-const defaultignore = ['build', 'es', 'dist', 'lib', 'node_modules', 'public', 'test', '__tests__'];
+const defaultignore = ['build', 'es', 'dist', 'lib', 'node_modules', 'demo', 'public', 'test', '__tests__'];
 // Support file exts
 const defaultSupportExts = ['js', 'jsx', 'ts', 'tsx'];
 
