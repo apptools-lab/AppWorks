3cda38c82246b507556b3efe73894b1d0ae9148e
luhc228 <luhengchang228@gmail.com>
2020-09-11 10:24:44 +0800

    feat: menu service

---
+++ packages/menu-service/src/index.ts
@@ -0,0 +1,112 @@
+import * as path from 'path';
+import * as fse from 'fs-extra';
+import traverse from '@babel/traverse';
+import * as parser from '@babel/parser';
+import * as t from '@babel/types';
+import generate from '@babel/generator';
+import * as prettier from 'prettier';
+import { projectPath, LAYOUT_DIRECTORY } from '@iceworks/project-service';
+import { IMenuData, MenuType } from './types';
+
+const HEADER_MENU_CONFIG_VARIABLES = 'headerMenuConfig';
+const ASIDE_MENU_CONFIG_VARIABLES = 'asideMenuConfig';
+
+export async function createMenu(layoutName: string, data: IMenuData, menuType: MenuType) {
+  const menuConfigPath = path.join(projectPath, LAYOUT_DIRECTORY, layoutName, 'menuConfig.js');
+  const menuConfigAST = await getMenuConfigAST(menuConfigPath);
+  const { headerMenuConfig, asideMenuConfig }: { headerMenuConfig: IMenuData[]; asideMenuConfig: IMenuData[] } = getAllConfig(menuConfigAST);
+
+  if (menuType === 'headerMenuConfig') {
+    headerMenuConfig.push(data);
+  } else if (menuType === 'asideMenuConfig') {
+    asideMenuConfig.push(data)
+  }
+
+  generateCode(headerMenuConfig, asideMenuConfig, menuConfigAST, menuConfigPath)
+}
+
+/**
+ * get header menu configs and aside menu configs 
+ * @param menuConfigAST 
+ */
+export function getAllConfig(menuConfigAST: t.File) {
+  let headerMenuConfig = [];
+  let asideMenuConfig = [];
+  traverse(menuConfigAST, {
+    VariableDeclarator: ({ node }) => {
+      // find headerMenuConfig
+      if (t.isIdentifier(node.id, { name: HEADER_MENU_CONFIG_VARIABLES }) && t.isArrayExpression(node.init)) {
+        headerMenuConfig = parseMenuConfig(node.init.elements)
+      }
+      // find asideMenuConfig
+      if (t.isIdentifier(node.id, { name: ASIDE_MENU_CONFIG_VARIABLES }) && t.isArrayExpression(node.init)) {
+        asideMenuConfig = parseMenuConfig(node.init.elements)
+      }
+    },
+  });
+  return { headerMenuConfig, asideMenuConfig }
+}
+
+async function getMenuConfigAST(menuConfigPath: string) {
+  const menuConfigString = await fse.readFile(menuConfigPath, 'utf-8');
+  const menuConfigAST = getASTByCode(menuConfigString);
+  return menuConfigAST;
+}
+
+function getASTByCode(code: string) {
+  return parser.parse(code, {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    plugins: ['dynamicImport'],
+  })
+}
+
+function parseMenuConfig(elements: any[]) {
+  const config = [];
+  elements.forEach((element) => {
+    const { properties } = element;
+    const item = {} as IMenuData;
+    properties.forEach((property) => {
+      const { key, value } = property;
+      const { name: keyName } = key;
+      if (keyName === 'children') {
+        item.children = parseMenuConfig(value.elements);
+      } else {
+        item[keyName] = value.value
+      }
+    });
+    config.push(item)
+  })
+  return config;
+}
+
+function generateCode(headerMenuConfig: IMenuData[], asideMenuConfig: IMenuData[], menuConfigAST: any, menuConfigPath: string) {
+  const headerMenuConfigAST = getASTByCode(JSON.stringify(headerMenuConfig));
+  const asideMenuConfigAST = getASTByCode(JSON.stringify(asideMenuConfig));
+  traverse(menuConfigAST, {
+    VariableDeclarator: ({ node }) => {
+      // set headerMenuConfig
+      if (t.isIdentifier(node.id, { name: HEADER_MENU_CONFIG_VARIABLES }) && t.isArrayExpression(node.init)) {
+        node.init = headerMenuConfigAST.program.body[0] as any;
+      }
+      // set asideMenuConfig
+      if (t.isIdentifier(node.id, { name: ASIDE_MENU_CONFIG_VARIABLES }) && t.isArrayExpression(node.init)) {
+        node.init = asideMenuConfigAST.program.body[0] as any;
+      }
+    },
+  });
+  const result = formatCodeFromAST(menuConfigAST);
+  fse.writeFileSync(menuConfigPath, result);
+}
+
+function formatCodeFromAST(ast: t.Node) {
+  return prettier.format(
+    generate(ast, {
+      retainLines: true,
+    }).code,
+    {
+      singleQuote: true,
+      trailingComma: 'es5',
+    }
+  );
+}
