ee45f600e6f872f2b6875f2279a4ab474af79337
LouisLv <1121264060@qq.com>
2020-10-23 18:09:46 +0800

    feat: 优化打包逻辑，不暴露无关信息 (#631)
    * chore: 修正打包
    
    * fix: 标准化打包逻辑
    
    * chore: 移除无用脚本
    
    Co-authored-by: xunhe.lzy <xunhe.lzy@alibaba-inc.com>

--- extensions/iceworks/src/node/index.ts
+++ extensions/iceworks/src/node/index.ts
@@ -1,8 +1,8 @@
-import { IDEKit } from '@ali/kit-runner';
 import { activate as uiBuilderActivate } from '@ali/ide-extensions-iceworks-ui-builder';
 import { activate as projectActivate } from '@ali/ide-extensions-iceworks-project-creator';
 
-export default class KitNode extends IDEKit.IKitNodeBase {
+// TODO: 支持独立安装的插件形式
+export default class KitNode {
   activate(context) {
     uiBuilderActivate(context);
     projectActivate(context);
