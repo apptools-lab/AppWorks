76aa9c045a9539e2e58581604b0915127990163a
alvinhui <alvin.hui@qq.com>
2020-10-23 15:48:26 +0800

    fix: build

--- extensions/iceworks/src/node/index.ts
+++ extensions/iceworks/src/node/index.ts
@@ -1,11 +1,11 @@
 import { IDEKit } from '@ali/kit-runner';
-import { activate as uiBuilderActivate, deactivate as uiBuilderDeactivate } from '@ali/ide-extensions-iceworks-ui-builder';
-import { activate as projectActivate, deactivate as projectDeactivate } from '@ali/ide-extensions-iceworks-project-creator';
+import { activate as uiBuilderActivate } from '@ali/ide-extensions-iceworks-ui-builder';
+import { activate as projectActivate } from '@ali/ide-extensions-iceworks-project-creator';
 
 // TODO: 可选打包成单文件
 export default class KitNode extends IDEKit.IKitNodeBase {
   activate(context) {
-    uiBuilderDeactivate(context);
+    uiBuilderActivate(context);
     projectActivate(context);
   }
   deactivate(context) {
