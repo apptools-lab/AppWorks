4b33288d66f58712020073ec2b5e328d15bb5276
alvinhui <alvin.hui@qq.com>
2020-10-29 18:10:09 +0800

    feat: activateWalkClock

--- extensions/iceworks-time-master/src/extension.ts
+++ extensions/iceworks-time-master/src/extension.ts
@@ -2,13 +2,17 @@ import { ExtensionContext, commands } from 'vscode';
 import { createTimerTreeView } from './timerProvider';
 import { openFileInEditor } from './utils/common';
 import { createInstance as createKpmInstance } from './managers/kpm';
+import { activate as activateWalkClock } from './managers/walkClock';
 
 // eslint-disable-next-line
 const { name } = require('../package.json');
+
 export async function activate(context: ExtensionContext) {
   const { subscriptions } = context;
 
   createTimerTreeView();
+
+  activateWalkClock();
 
   const kpmInstance = createKpmInstance();
   kpmInstance.activate();
