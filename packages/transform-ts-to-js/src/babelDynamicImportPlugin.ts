e6a9704c2646e01d943ea164723658f32c66f3ce
Hengchang Lu <44047106+luhc228@users.noreply.github.com>
2020-11-05 13:57:59 +0800

    chore: migrate transform-ts-to-js package (#641)
    * chore: migrate transform-ts-to-js package
    
    * chore: remove reference in tsconfig.json

--- packages/transform-ts-to-js/src/babelDynamicImportPlugin.ts
+++
@@ -1,9 +0,0 @@
-import DynamicImport from '@babel/plugin-syntax-dynamic-import';
-
-export default function () {
-  return {
-    name: 'syntax-dynamic-import',
-    inherits: DynamicImport,
-    visitor: {},
-  };
-}
