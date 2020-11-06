1ce05e44d1b266bfbda15bec206d57dae0e27408
alvinhui <alvin.hui@qq.com>
2020-11-06 14:56:48 +0800

    feat: dashboard for utils

--- extensions/iceworks-time-master/src/storages/user.ts
+++ extensions/iceworks-time-master/src/storages/user.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as fse from 'fs-extra';
 import * as moment from 'moment';
 import { getAppDataDir, getAppDataDayDir } from '../utils/common';
+import { getDashboardRow, getRangeDashboard, getDashboardHr } from '../utils/dashboard';
 
 export class UserSummary {
   /**
@@ -63,85 +64,6 @@ export function updateUserSummary(user: UserSummary) {
 
 export function getUserDashboardFile() {
   return path.join(getAppDataDir(), 'UserSummaryDashboard.txt');
-}
-
-const DASHBOARD_LABEL_WIDTH = 28;
-const DASHBOARD_VALUE_WIDTH = 36;
-const DASHBOARD_SEPARATOR = ' :';
-function getSpaces(spacesRequired: number) {
-  let spaces = '';
-  if (spacesRequired > 0) {
-    for (let i = 0; i < spacesRequired; i++) {
-      spaces += ' ';
-    }
-  }
-  return spaces;
-}
-function getDashboardValue(value: string) {
-  const spacesRequired = DASHBOARD_VALUE_WIDTH - value.length - 2;
-  const spaces = getSpaces(spacesRequired);
-  return `  ${spaces}${value}`;
-}
-function getDashboardLabel(label: string) {
-  const spacesRequired = DASHBOARD_LABEL_WIDTH - label.length;
-  const spaces = getSpaces(spacesRequired);
-  return `${spaces}${label}`;
-}
-
-function getDashboardHr() {
-  let content = '';
-  const dashLen = DASHBOARD_LABEL_WIDTH + DASHBOARD_VALUE_WIDTH + DASHBOARD_SEPARATOR.length;
-  for (let i = 0; i < dashLen; i++) {
-    content += '-';
-  }
-  content += '\n';
-  return content;
-}
-
-function getDashboardRow(label: string, value: string) {
-  const dashboardLabel = getDashboardLabel(label);
-  const dashboardValue = getDashboardValue(value);
-  const content = `${dashboardLabel}${DASHBOARD_SEPARATOR}${dashboardValue}\n`;
-  return content;
-}
-
-function getRangeDashboard(title: string) {
-  let str = `${title}\n`;
-  const hrStr = getDashboardHr();
-  str += hrStr;
-  str += getDashboardRow(
-    'Code time',
-    '7 min',
-  );
-  str += getDashboardRow(
-    'Lines of code added',
-    '405',
-  );
-  str += getDashboardRow(
-    'Lines of code deleted',
-    '292',
-  );
-  str += getDashboardRow(
-    'Characters added',
-    '15,397',
-  );
-  str += getDashboardRow(
-    'Characters deleted',
-    '12,095',
-  );
-  str += getDashboardRow(
-    'Total keystrokes',
-    '3,049',
-  );
-  str += getDashboardRow(
-    'KPM',
-    '10',
-  );
-  str += getDashboardRow(
-    'Top language',
-    'typescript',
-  );
-  return str;
 }
 
 export async function generateUserDashboard() {
