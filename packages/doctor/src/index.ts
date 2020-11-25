b8f25f1f5b7e55a94868397dd8fa3ce62971c5f0
yangfan <18767120422@163.com>
2020-11-25 15:59:49 +0800

    feat: doctor add codemod (#662)
    * feat: add transforms
    
    * feat: update description

--- packages/doctor/src/index.ts
+++ packages/doctor/src/index.ts
@@ -1,6 +1,7 @@
+import getFiles from './getFiles';
 import Scanner from './Scanner';
 import { IDoctorOptions } from './types/Doctor';
-import { IScanOptions, IScannerReports } from './types/Scanner';
+import { IFileInfo, IScanOptions, IScannerReports } from './types/Scanner';
 
 // Ignore directories
 const defaultignore = ['build', 'es', 'dist', 'lib', 'mocks', 'coverage', 'node_modules', 'demo', 'examples', 'public', 'test', '__tests__'];
@@ -9,19 +10,28 @@ const defaultSupportExts = ['js', 'jsx', 'ts', 'tsx'];
 
 class Doctor {
   public options: any;
-
+  public ignore: string[];
+  public supportExts: string[];
   private scanner: any;
 
   constructor(options: IDoctorOptions) {
     this.options = options || {};
+
+    this.ignore = defaultignore.concat(this.options.ignore || []);
+    this.supportExts = defaultSupportExts.concat(this.options.supportExts || []);
+
     this.scanner = new Scanner({
-      ignore: defaultignore.concat(this.options.ignore || []),
-      supportExts: defaultSupportExts.concat(this.options.supportExts || []),
+      ignore: this.ignore,
+      supportExts: this.supportExts,
     });
   }
 
   scan(directory: string, options?: IScanOptions): Promise<IScannerReports> {
     return this.scanner.scan(directory, options);
+  }
+
+  getFiles(directory: string): IFileInfo[] {
+    return getFiles(directory, this.supportExts, this.ignore);
   }
 }
 
