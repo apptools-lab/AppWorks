58c0d46e175e57344270bbc0617c06425c1d344a
yangfan <18767120422@163.com>
2020-12-15 15:37:05 +0800

    feat: update preview

--- extensions/iceworks-app/src/utils/createEditorMenuAction.ts
+++ extensions/iceworks-app/src/utils/createEditorMenuAction.ts
@@ -1,7 +1,10 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as vscode from 'vscode';
 import { createNpmCommand, checkPathExists, checkIsAliInternal, registerCommand } from '@iceworks/common-service';
-import { dependencyDir, getProjectFramework, projectPath } from '@iceworks/project-service';
+import { checkIsPegasusProject, dependencyDir, getProjectFramework, projectPath } from '@iceworks/project-service';
 import { connectService, getHtmlForWebview } from '@iceworks/vscode-webview/lib/vscode';
+import { DEFAULT_START_URL, IDevServerStartInfo, getDevServerStartInfo } from './getDevServerStartInfo';
 import services from '../services';
 import showDefPublishEnvQuickPick from '../quickPicks/showDefPublishEnvQuickPick';
 import executeCommand from '../commands/executeCommand';
@@ -9,8 +12,36 @@ import executeCommand from '../commands/executeCommand';
 let previewWebviewPanel: vscode.WebviewPanel | undefined;
 
 export default async function createEditorMenuAction(context: vscode.ExtensionContext, recorder) {
+  const { window } = vscode;
+  const { extensionPath } = context;
+
+  function openPreview(startInfo?: IDevServerStartInfo) {
+    if (previewWebviewPanel) {
+      previewWebviewPanel.reveal();
+    }
+
+    previewWebviewPanel = window.createWebviewPanel('iceworks', 'Preview', vscode.ViewColumn.Two, {
+      enableScripts: true,
+      retainContextWhenHidden: true,
+    });
+
+    const extraHtml = `<script>window.__PREVIEW__DATA__ = ${JSON.stringify(startInfo || { startUrl: DEFAULT_START_URL })};</script>`;
+
+    previewWebviewPanel.webview.html = getHtmlForWebview(extensionPath, 'preview', false, undefined, extraHtml);
+    previewWebviewPanel.onDidDispose(
+      () => {
+        previewWebviewPanel = undefined;
+      },
+      null,
+      context.subscriptions,
+    );
+    connectService(previewWebviewPanel, context, { services, recorder });
+  }
+
   const EDITOR_MENU_RUN_DEBUG = 'iceworksApp.editorMenu.runDebug';
   registerCommand(EDITOR_MENU_RUN_DEBUG, async () => {
+    const isPegasusProject = await checkIsPegasusProject();
+
     // Check dependences
     if (!(await checkPathExists(projectPath, dependencyDir))) {
       vscode.window.showInformationMessage('"node_modules" directory not found! Install dependencies first.');
@@ -22,51 +53,35 @@ export default async function createEditorMenuAction(context: vscode.ExtensionCo
       return;
     }
 
+    // npm run start.
+    // Debug in VS Code move to iceworks docs.
+    executeCommand({
+      command: EDITOR_MENU_RUN_DEBUG,
+      title: 'Run Start',
+      arguments: [projectPath, createNpmCommand('run', 'start')],
+    });
+
     if (await getProjectFramework() === 'rax-app') {
-      // Rax project preview
-      const { window } = vscode;
-      const { extensionPath } = context;
-
-      executeCommand({
-        command: EDITOR_MENU_RUN_DEBUG,
-        title: 'Run Start',
-        arguments: [projectPath, createNpmCommand('run', 'start', '-- --disable-open')],
-      });
-
-      setTimeout(() => {
-        if (previewWebviewPanel) {
-          previewWebviewPanel.reveal();
+      const devServerStartInfo: IDevServerStartInfo = await getDevServerStartInfo(projectPath);
+      openPreview(devServerStartInfo);
+    } else if (isPegasusProject) {
+      // Set pegasus service url
+      try {
+        const abcConfigFile = path.join(projectPath, 'abc.json');
+        if (fs.existsSync(abcConfigFile)) {
+          const abcConfig = fs.readJSONSync(abcConfigFile);
+          if (abcConfig.type === 'pegasus' && abcConfig.group && abcConfig.name) {
+            openPreview({ startUrl: `${DEFAULT_START_URL}${abcConfig.group}/${abcConfig.name}` });
+          }
         }
-
-        previewWebviewPanel = window.createWebviewPanel('iceworks', 'Preview', vscode.ViewColumn.Two, {
-          enableScripts: true,
-          retainContextWhenHidden: true,
-        });
-
-        const extraHtml = `<script>
-          window.__PREVIEW__DATA__ = { startUrl: 'http://localhost:3333/' };
-        </script>
-        `;
-        previewWebviewPanel.webview.html = getHtmlForWebview(extensionPath, 'preview', false, undefined, extraHtml);
-        previewWebviewPanel.onDidDispose(
-          () => {
-            previewWebviewPanel = undefined;
-          },
-          null,
-          context.subscriptions,
-        );
-        connectService(previewWebviewPanel, context, { services, recorder });
-      }, 5000);
-    } else {
-      // npm run start.
-      // Debug in VS Code move to iceworks docs.
-      executeCommand({
-        command: EDITOR_MENU_RUN_DEBUG,
-        title: 'Run Start',
-        arguments: [projectPath, createNpmCommand('run', 'start')],
-      });
+      } catch (e) {
+        // ignore
+      }
     }
   });
+
+  const PREVIEW_OPEN = 'iceworksApp.preview.open';
+  registerCommand(PREVIEW_OPEN, openPreview);
 
   const EDITOR_MENU_RUN_BUILD = 'iceworksApp.editorMenu.runBuild';
   registerCommand(EDITOR_MENU_RUN_BUILD, async () => {
--- extensions/iceworks-app/web/src/pages/Preview/components/QRCodeWrap/index.tsx
+++ extensions/iceworks-app/web/src/pages/Preview/components/QRCodeWrap/index.tsx
@@ -6,22 +6,50 @@ interface IProps {
   url: string
 }
 
+// Don't show MiniApp urls
+const KEY_MAP = {
+  web: { title: 'Web' }, // Primary key
+  kraken: { title: 'Kraken' },
+  weex: { title: 'Weex' },
+};
+
+function getCurrentQRCodeInfo(url: string) {
+  const currentQRCodeInfo = [];
+  const { startQRCodeInfo } = window.__PREVIEW__DATA__;
+
+  if (startQRCodeInfo) {
+    const primaryIndex = (startQRCodeInfo.web || []).findIndex(webUrl => url === webUrl);
+
+    // If web dev server url exist render QRCode
+    if (primaryIndex > -1) {
+      Object.keys(startQRCodeInfo).forEach((infoKey) => {
+        if (KEY_MAP[infoKey]) {
+          currentQRCodeInfo.push({
+            title: KEY_MAP[infoKey].title,
+            // Rax project can have different mpa targets, spa only have one preview url
+            value: startQRCodeInfo[infoKey][primaryIndex] || startQRCodeInfo[infoKey][0],
+          });
+        }
+      });
+    }
+  }
+  return currentQRCodeInfo;
+}
+
 export default function (props: IProps) {
   const { url } = props;
-  const { startUrl, startQRCodeInfo } = window.__PREVIEW__DATA__;
+  const currentQRCodeInfo = getCurrentQRCodeInfo(url);
 
   return (
     <div className={styles.container}>
-      {startUrl === url && startQRCodeInfo ? (
-        Object.keys(startQRCodeInfo).map((title) => {
-          return (
-            <div className={styles.wrap}>
-              <h3>{title}：</h3>
-              <QRCode value={startQRCodeInfo[title]} />
-            </div>
-          );
-        })
-      ) : <QRCode value={url} />}
+      { currentQRCodeInfo.length > 0 ? (currentQRCodeInfo.map((QRCodeInfo) => {
+        return (
+          <div className={styles.wrap} key={QRCodeInfo.title}>
+            <h3>{QRCodeInfo.title}：</h3>
+            <QRCode value={QRCodeInfo.value} />
+          </div>
+        );
+      })) : <QRCode value={url} />}
     </div>
   );
 }
