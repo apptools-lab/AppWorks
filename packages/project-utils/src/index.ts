d628e1465c8d6b1b1dedb84de2698e41366528a3
alvinhui <alvin.hui@qq.com>
2020-12-29 10:44:43 +0800

    feat: checkIsTargetProjectFramework

--- packages/project-utils/src/index.ts
+++ packages/project-utils/src/index.ts
@@ -1,12 +1,15 @@
 import { readFile } from 'jsonfile';
 import { join } from 'path';
 
+const packageJSONFilename = 'package.json';
+
 export type ProjectType = 'unknown'|'rax'|'react'|'vue';
+export type ProjectFramework = 'unknown'|'rax-app'|'icejs'|'vue';
 
 export async function getProjectType(projectPath: string): Promise<ProjectType> {
   let type: ProjectType = 'unknown';
   try {
-    const packageJsonPath = join(projectPath, 'package.json');
+    const packageJsonPath = join(projectPath, packageJSONFilename);
     const { dependencies = {} } = await readFile(packageJsonPath);
     if (dependencies.rax) {
       type = 'rax';
@@ -23,6 +26,31 @@ export async function getProjectType(projectPath: string): Promise<ProjectType> 
   return type;
 }
 
+export async function getProjectFramework(projectPath: string): Promise<ProjectFramework> {
+  let framework: ProjectFramework = 'unknown';
+  try {
+    const packageJsonPath = join(projectPath, packageJSONFilename);
+    const { dependencies = {}, devDependencies = {} } = await readFile(packageJsonPath);
+    if (devDependencies['rax-app'] || dependencies['rax-app']) {
+      framework = 'rax-app';
+    }
+    if (devDependencies['ice.js'] || dependencies['ice.js']) {
+      framework = 'icejs';
+    }
+    if (dependencies.vue) {
+      framework = 'vue';
+    }
+  } catch (error) {
+    // ignore errors
+  }
+
+  return framework;
+}
+
 export async function checkIsTargetProject(projectPath: string): Promise<boolean> {
   return (await getProjectType(projectPath)) !== 'unknown';
+}
+
+export async function checkIsTargetProjectFramework(projectPath: string): Promise<boolean> {
+  return (await getProjectFramework(projectPath)) !== 'unknown';
 }
