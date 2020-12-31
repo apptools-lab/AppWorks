bf37af8c84f955d36a528679ed3a56087aba5844
yangfan <jason.yf@alibaba-inc.com>
2020-12-31 16:18:02 +0800

    fix: publish

--- scripts/publish-extension.ts
+++ scripts/publish-extension.ts
@@ -10,7 +10,12 @@ import checkPackagePublished from './fn/checkPackagePublished';
 import extensionDepsInstall from './fn/extension-deps-install';
 import updateExtensionDependencies from './fn/updateExtensionDependencies';
 
+const SKIP_PACK_EXTENSION_LIST = ['iceworks-doctor'];
+
 function packExtension(extension: string, directory: string, version: string) {
+  if (SKIP_PACK_EXTENSION_LIST.indexOf(extension) > -1) {
+    return;
+  }
   console.log('[VSCE] PACK: ', `${extension}@${version}`);
   spawnSync('vsce', ['package'], {
     stdio: 'inherit',
