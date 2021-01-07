b31c7ccdc86d177a5ed2c8479c3fe5b27263d304
alvinhui <alvin.hui@qq.com>
2021-01-07 14:37:41 +0800

    feat: dashboard web

---
+++ extensions/iceworks-app/web/src/pages/Dashboard/components/Framework/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export default () => {
+  return (
+    <div className={styles.container}>
+      framework
+    </div>
+  );
+};
---
+++ extensions/iceworks-app/web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { LocaleProvider } from '@/i18n';
+import Basic from './components/Basic';
+import Doctor from './components/Doctor';
+import Framework from './components/Framework';
+import styles from './index.module.scss';
+
+export default () => {
+  return (
+    <LocaleProvider>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <Basic />
+        </div>
+        <div className={styles.box}>
+          <Doctor />
+        </div>
+        <div className={styles.box}>
+          <Framework />
+        </div>
+      </div>
+    </LocaleProvider>
+  );
+};
