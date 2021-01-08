27c285c5baa8fa36ce108412bf0becad91485645
alvinhui <alvin.hui@qq.com>
2021-01-08 17:29:04 +0800

    feat: add Doctor to Dashboard

--- extensions/iceworks-app/web/src/pages/Dashboard/components/Doctor/index.tsx
+++ extensions/iceworks-app/web/src/pages/Dashboard/components/Doctor/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import DoctorDashboard from '@iceworks/doctor-ui';
 import styles from './index.module.scss';
 
 export default () => {
   return (
     <div className={styles.container}>
-      doctor
+      <h3>Docotr</h3>
+      <div>
+        <DoctorDashboard filesInfo={{ lines: 0, count: 0 }} score={0} />
+      </div>
     </div>
   );
 };
