8035cc94f39e31cb34d2b1a05d5ffb65463f0aff
alvinhui <alvin.hui@qq.com>
2021-01-10 22:55:32 +0800

    chore: style

--- extensions/iceworks-app/web/src/pages/Dashboard/components/Doctor/index.tsx
+++ extensions/iceworks-app/web/src/pages/Dashboard/components/Doctor/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.scss';
 export default () => {
   return (
     <div className={styles.container}>
-      <h3>Docotr</h3>
+      <h2>质量信息</h2>
       <div>
         <DoctorDashboard filesInfo={{ lines: 0, count: 0 }} score={0} />
       </div>
