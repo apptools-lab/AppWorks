d3a2283a4ca070370c23094f9f34073d0ba4f504
yangfan <18767120422@163.com>
2021-02-02 12:01:36 +0800

    feat: update review

--- packages/project-utils/src/index.ts
+++ packages/project-utils/src/index.ts
@@ -1,18 +1,25 @@
 import { join } from 'path';
-import { readFileSync, existsSync } from 'fs';
+import { exists, readFile } from 'fs';
+import { promisify } from 'util';
+
+const existsAsync = promisify(exists);
+const readFileAsync = promisify(readFile);
 
 const packageJSONFilename = 'package.json';
 
 export type ProjectType = 'unknown' | 'rax' | 'react' | 'vue';
 export type ProjectFramework = 'unknown' | 'rax-app' | 'icejs' | 'rax-component' | 'react-component' | 'pegasus-module' | 'vue';
+export type ProjectLanguageType = 'ts' | 'js';
+export type ProjectTypeInfo = { type: ProjectType, version: string };
+export type ProjectFrameworkInfo = { framework: ProjectFramework, version: string };
 
-function getProjectTypeInfo(projectPath: string): { type: ProjectType, version: string } {
+async function getProjectTypeInfo(projectPath: string): Promise<ProjectTypeInfo> {
   let type: ProjectType = 'unknown';
   let version = 'unknown';
 
   try {
     const packageJsonPath = join(projectPath, packageJSONFilename);
-    const { dependencies = {}, peerDependencies = {} } = JSON.parse(readFileSync(packageJsonPath, { encoding: 'utf-8' }));
+    const { dependencies = {}, peerDependencies = {} } = JSON.parse(await readFileAsync(packageJsonPath, 'utf-8'));
 
     if (dependencies.rax || peerDependencies.rax) {
       type = 'rax';
@@ -31,13 +38,13 @@ function getProjectTypeInfo(projectPath: string): { type: ProjectType, version: 
   return { type, version };
 }
 
-function getProjectFrameworkInfo(projectPath: string): { framework: ProjectFramework, version: string } {
+async function getProjectFrameworkInfo(projectPath: string): Promise<ProjectFrameworkInfo> {
   let framework: ProjectFramework = 'unknown';
   let version = 'unknown';
 
   try {
     const packageJsonPath = join(projectPath, packageJSONFilename);
-    const { dependencies = {}, devDependencies = {} } = JSON.parse(readFileSync(packageJsonPath, { encoding: 'utf-8' }));
+    const { dependencies = {}, devDependencies = {} } = JSON.parse(await readFileAsync(packageJsonPath, 'utf-8'));
 
     if (devDependencies['rax-app'] || dependencies['rax-app']) {
       framework = 'rax-app';
@@ -54,7 +61,7 @@ function getProjectFrameworkInfo(projectPath: string): { framework: ProjectFrame
     }
     if (devDependencies['build-plugin-component']) {
       const buildJsonPath = join(projectPath, 'build.json');
-      const buildConfig = existsSync(buildJsonPath) ? JSON.parse(readFileSync(buildJsonPath, { encoding: 'utf-8' })) : {};
+      const buildConfig = await existsAsync(buildJsonPath) ? JSON.parse(await readFileAsync(buildJsonPath, 'utf-8')) : {};
 
       if (buildConfig.type === 'rax') {
         framework = 'rax-component';
@@ -66,8 +73,8 @@ function getProjectFrameworkInfo(projectPath: string): { framework: ProjectFrame
     }
 
     const abcJsonPath = join(projectPath, 'abc.json');
-    if (existsSync(abcJsonPath) && devDependencies['@ali/build-plugin-pegasus-base']) {
-      const abcConfig = JSON.parse(readFileSync(abcJsonPath, { encoding: 'utf-8' }));
+    if (await existsAsync(abcJsonPath) && devDependencies['@ali/build-plugin-pegasus-base']) {
+      const abcConfig = JSON.parse(await readFileAsync(abcJsonPath, 'utf-8'));
       if (abcConfig.builder === '@ali/builder-pegasus') {
         framework = 'pegasus-module';
         version = devDependencies['@ali/build-plugin-pegasus-base'];
@@ -85,33 +92,35 @@ function getProjectFrameworkInfo(projectPath: string): { framework: ProjectFrame
 }
 
 export async function getProjectType(projectPath: string): Promise<ProjectType> {
-  return getProjectTypeInfo(projectPath).type;
+  const { type } = await getProjectTypeInfo(projectPath);
+  return type;
 }
 
 export async function getProjectFramework(projectPath: string): Promise<ProjectFramework> {
-  return getProjectFrameworkInfo(projectPath).framework;
+  const { framework } = await getProjectFrameworkInfo(projectPath);
+  return framework;
 }
 
-export function getProjectTypeAndVersionSync(projectPath: string): { type: ProjectType, version: string } {
+export async function getProjectTypeAndVersion(projectPath: string): Promise<ProjectTypeInfo> {
   return getProjectTypeInfo(projectPath);
 }
 
-export function getProjectFrameworkAndVersionSync(projectPath: string): { framework: ProjectFramework, version: string } {
+export async function getProjectFrameworkAndVersionSync(projectPath: string): Promise<ProjectFrameworkInfo> {
   return getProjectFrameworkInfo(projectPath);
 }
 
-export function getProjectLanguageTypeSync(projectPath: string): 'ts' | 'js' {
-  const hasTsconfig = existsSync(join(projectPath, 'tsconfig.json'));
+export async function getProjectLanguageTypeSync(projectPath: string): Promise<ProjectLanguageType> {
+  const hasTsconfig = await existsAsync(join(projectPath, 'tsconfig.json'));
 
-  const { framework, version } = getProjectFrameworkAndVersionSync(projectPath);
+  const { framework, version } = await getProjectFrameworkAndVersionSync(projectPath);
   let isTypescript = false;
   if (framework === 'icejs') {
     // icejs 都有 tsconfig，因此需要通过 src/app.js[x] 进一步区分
-    const hasAppJs = existsSync(join(projectPath, 'src/app.js')) || existsSync(join(projectPath, 'src/app.jsx'));
+    const hasAppJs = await existsAsync(join(projectPath, 'src/app.js')) || await existsAsync(join(projectPath, 'src/app.jsx'));
     isTypescript = hasTsconfig && !hasAppJs;
   } else if (framework === 'rax-app' && version.indexOf('^3.') === 0) {
     // rax-app@3.x 都有 tsconfig，因此需要通过 src/app.js[x] 进一步区分
-    const hasAppJs = existsSync(join(projectPath, 'src/app.js')) || existsSync(join(projectPath, 'src/app.jsx'));
+    const hasAppJs = await existsAsync(join(projectPath, 'src/app.js')) || await existsAsync(join(projectPath, 'src/app.jsx'));
     isTypescript = hasTsconfig && !hasAppJs;
   } else {
     isTypescript = hasTsconfig;
