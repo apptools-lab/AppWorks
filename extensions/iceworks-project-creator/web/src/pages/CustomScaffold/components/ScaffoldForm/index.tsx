8c733b3a3b6ef0f695652256e84ad4b8d1e08cfa
luhc228 <luhengchang228@gmail.com>
2021-02-26 19:52:01 +0800

    fix: style

--- extensions/iceworks-project-creator/web/src/pages/CustomScaffold/components/ScaffoldForm/index.tsx
+++ extensions/iceworks-project-creator/web/src/pages/CustomScaffold/components/ScaffoldForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Drawer, Icon } from '@alifd/next';
+import { Icon } from '@alifd/next';
 import LayoutConfig from '../LayoutConfig';
 import { Base64 } from 'js-base64';
 import styles from './index.module.scss';
@@ -7,14 +7,10 @@ import styles from './index.module.scss';
 const Scaffoldform = ({ children, onChange, scaffoldValue }) => {
   const iframeRef = useRef(null);
 
-  const [drawerVisible, setDrawerVisible] = useState<boolean>(false);
+  const [drawerVisible, setDrawerVisible] = useState<boolean>(true);
 
-  const onDrawerOpen = () => {
-    setDrawerVisible(true);
-  };
-
-  const onDrawerClose = () => {
-    setDrawerVisible(false);
+  const onToggle = () => {
+    setDrawerVisible(!drawerVisible);
   };
 
   const sendMessage = (content: string) => {
@@ -35,18 +31,17 @@ const Scaffoldform = ({ children, onChange, scaffoldValue }) => {
     sendMessage(JSON.stringify(scaffoldValue));
   }, [scaffoldValue]);
   return (
-    <div className={styles.scaffoldScaffold}>
+    <div className={styles.scaffoldForm}>
       <div className={styles.content}>
         <iframe ref={iframeRef} className={styles.scaffoldTemplateIframe} frameBorder="0" name="scaffoldTemplate" />
-        <div onClick={() => onDrawerOpen()} className={styles.drawerBtn}><Icon type="set" size="large" /></div>
-        <Drawer
-          visible={drawerVisible}
-          placement="right"
-          width={360}
-          onClose={onDrawerClose}
-        >
-          <LayoutConfig value={scaffoldValue} onChange={onChange} />
-        </Drawer>
+        <div onClick={() => onToggle()} className={styles.drawerBtn}><Icon type="set" size="large" /></div>
+        {
+          drawerVisible && (
+          <div className={styles.config}>
+            <LayoutConfig value={scaffoldValue} onChange={onChange} />
+          </div>
+          )
+        }
       </div>
       <div className={styles.action}>{children}</div>
     </div>
