454bfa05f574546a9b2a13c053680e103e716eda
luhc228 <luhengchang228@gmail.com>
2021-03-03 18:59:26 +0800

    fix: switch component default param

--- extensions/iceworks-project-creator/web/src/components/CustomSwitch/index.tsx
+++ extensions/iceworks-project-creator/web/src/components/CustomSwitch/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch } from '@alifd/next';
 
 const SwitchComponent = ({ checked, onChange, size }) => (
-  <Switch checked={checked} onChange={onChange} size={size} />
+  <Switch checked={checked} onChange={onChange} size={size || 'medium'} />
 );
 
 export default SwitchComponent;
