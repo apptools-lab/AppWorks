4833d9a6269b4924e9ab73606f60c84c5834f97d
alvinhui <alvin.hui@qq.com>
2021-03-12 12:30:10 +0800

    refactor: using command to get user info

--- packages/user-service/src/index.ts
+++ packages/user-service/src/index.ts
@@ -1,18 +1,18 @@
-import { IUserInfo } from './types';
 import { ALI_DEF_URL } from '@iceworks/constant';
 import configure from '@iceworks/configure';
+import * as vscode from 'vscode';
+import { IUserInfo } from './types';
 
 // eslint-disable-next-line
 const co = require('co');
 
 const CONFIGURE_USER_KEY = 'user';
+const isO2 = !!process.env.O2_VERSION;
 
-let Client;
 let defClient;
-
 try {
   /* eslint-disable */
-  Client = require('../def-login-client');
+  const Client = require('../def-login-client');
   defClient = new Client({
     server: ALI_DEF_URL,
   });
@@ -20,16 +20,20 @@ try {
   console.log('def-login-client is not found');
 }
 
-export async function getUserInfo(): Promise<IUserInfo> {
-  const fn = co.wrap(function* () {
-    if (defClient) {
-      const user = yield defClient.user();
-      return user;
-    } else {
-      throw new Error('Error: Fail to get user info through def client.');
-    }
-  });
+const getUserInfoFromDefClient = co.wrap(function* () {
+  if (defClient) {
+    const user = yield defClient.user();
+    return user;
+  } else {
+    throw new Error('Error: Fail to get user info through def client.');
+  }
+});
 
+const getUserInfoFromCommand = async function() {
+  return await vscode.commands.executeCommand('core.account.get');
+}
+
+export async function getUserInfo(): Promise<IUserInfo> {
   // get user info from setting.json
   const userData = configure.get(CONFIGURE_USER_KEY) || {};
   const { empId, account, gitlabToken } = userData;
@@ -38,6 +42,7 @@ export async function getUserInfo(): Promise<IUserInfo> {
     return userData;
   } else {
     try {
+      const fn = isO2 ? getUserInfoFromCommand : getUserInfoFromDefClient;
       const { account, empid: empId } = await fn();
       const result = { account, empId, gitlabToken };
       configure.set(CONFIGURE_USER_KEY, result);
