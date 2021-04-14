9288d61d058aa347d69e6c97ad21c02fbc595a1b
alvinhui <alvin.hui@qq.com>
2021-04-14 14:16:52 +0800

    chore: more transform for react

--- extensions/iceworks-codemod/src/services/codeMod/react.ts
+++ extensions/iceworks-codemod/src/services/codeMod/react.ts
@@ -38,6 +38,21 @@ export default {
       description: 'Renames the experimental `unstable_handleError` lifecycle hook to `componentDidCatch`.',
     },
     {
+      name: 'findDOMNode',
+      filename: 'findDOMNode',
+      description: 'Updates `this.getDOMNode()` or `this.refs.foo.getDOMNode()` calls inside of `React.createClass` components to `React.findDOMNode(foo)`. Note that it will only look at code inside of `React.createClass` calls and only update calls on the component instance or its refs. You can use this script to update most calls to `getDOMNode` and then manually go through the remaining calls..',
+    },
+    {
+      name: 'Manual bind to Arrow',
+      filename: 'manual-bind-to-arrow',
+      description: 'Converts manual function bindings in a class (e.g., `this.f = this.f.bind(this)`) to arrow property initializer functions (e.g., `f = () => {}`).',
+    },
+    {
+      name: 'Pure Component',
+      filename: 'pure-component',
+      description: 'Converts ES6 classes that only have a render method, only have safe properties (statics and props), and do not have refs to Functional Components.',
+    },
+    {
       name: 'React PropTypes to prop-types',
       filename: 'React-PropTypes-to-prop-types',
       description: 'Replaces `React.PropTypes` references with `prop-types` and adds the appropriate `import` or `require` statement.',
@@ -48,24 +63,14 @@ export default {
       description: 'Adds "UNSAFE_" prefix for renamed lifecycle hooks.',
     },
     {
-      name: 'Manual bind to Arrow',
-      filename: 'manual-bind-to-arrow',
-      description: 'Converts manual function bindings in a class (e.g., `this.f = this.f.bind(this)`) to arrow property initializer functions (e.g., `f = () => {}`).',
-    },
-    {
-      name: 'findDOMNode',
-      filename: 'findDOMNode',
-      description: 'Updates `this.getDOMNode()` or `this.refs.foo.getDOMNode()` calls inside of `React.createClass` components to `React.findDOMNode(foo)`. Note that it will only look at code inside of `React.createClass` calls and only update calls on the component instance or its refs. You can use this script to update most calls to `getDOMNode` and then manually go through the remaining calls..',
+      name: 'React.DOM to React DOM Factories',
+      filename: 'React-DOM-to-react-dom-factories',
+      description: 'Converts calls like `React.DOM.div(...)` to `React.createElement(\'div\', ...)`.',
     },
     {
       name: 'sort-comp',
       filename: 'sort-comp',
       description: 'Reorders React component methods to match the ESLint react/sort-comp rule. (Defaults to ordering of the Airbnb style guide.',
     },
-    // {
-    //   name: 'Pure Component',
-    //   filename: 'pure-component',
-    //   description: 'Converts ES6 classes that only have a render method, only have safe properties (statics and props), and do not have refs to Functional Components.',
-    // },
   ],
 };
