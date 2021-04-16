11824e6a13ca044b841af4ddedc899c6a6b9ba98
alvinhui <alvin.hui@qq.com>
2021-04-16 15:52:44 +0800

    refactor: updated for review comments

--- extensions/iceworks-codemod/src/services/codeMod/index.ts
+++ extensions/iceworks-codemod/src/services/codeMod/index.ts
@@ -162,6 +162,7 @@ export async function getTransformsReport(transforms: TransForm[], codeModName: 
         '**/build/**',
         '**/.ice/**',
         '**/.rax/**',
+        '**/.faas_debug_tmp/**',
       ],
       nodir: true,
       dot: true,
