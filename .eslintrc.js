c8e2be0b0ec32a0618f4175a51c3d48a12424e08
alvinhui <alvin.hui@qq.com>
2021-04-22 15:47:35 +0800

    style: strings must use singlequote

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ const { getESLintConfig } = require('@iceworks/spec');
 // getESLintConfig(rule: 'rax'|'react'|'vue', customConfig?);
 module.exports = getESLintConfig('react-ts', {
   rules: {
-    "@typescript-eslint/member-delimiter-style": 1,
-    "max-len": 1,
-    "import/no-cycle": 1,
-    "@typescript-eslint/dot-notation": 1,
-    "@typescript-eslint/consistent-type-assertions": 1,
-    "@typescript-eslint/brace-style": 1,
-    "no-redeclare": 1,
+    '@typescript-eslint/member-delimiter-style': 1,
+    'max-len': 1,
+    'import/no-cycle': 1,
+    '@typescript-eslint/dot-notation': 1,
+    '@typescript-eslint/consistent-type-assertions': 1,
+    '@typescript-eslint/brace-style': 1,
+    'no-redeclare': 1,
   },
 });
