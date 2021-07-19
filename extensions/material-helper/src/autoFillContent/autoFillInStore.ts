32f47bc09f2bbc024fb318baea41d7a3f6c2d610
GiveMe-A-Name <a455300764b@hotmail.com>
2021-07-19 14:33:52 +0800

    refactor: variable rename

--- extensions/material-helper/src/autoFillContent/autoFillInStore.ts
+++ extensions/material-helper/src/autoFillContent/autoFillInStore.ts
@@ -8,15 +8,23 @@ import * as fs from 'fs';
 const renderFileAsync = util.promisify(ejs.renderFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-enum createFileTypes {
-  null,
-  store,
-  inModels,
+enum CreateFileTypes {
+  Null,
+  Store,
+  Models,
 }
 
 function checkIsTsorJsFile(filename: string) {
   return ['.js', '.ts'].includes(path.extname(filename));
 }
+
+
+/**
+ * check the file is or not validate Store.[t|j]s file
+ * conditions:
+ *  1. the filename must is store.[t|j]s
+ *  2. the filePath must start with xxx/src or xxx/src/pages/xxx;
+ */
 
 function checkIsValidateStoreFile(srcDir: string, filePath: string, filename: string): boolean {
   const pagesDir = 'src/pages';
@@ -36,7 +44,16 @@ function checkIsValidateStoreFile(srcDir: string, filePath: string, filename: st
   return storeFilenames.includes(filename) && isInValidateDir;
 }
 
+
+/**
+ * check the file is or not validate model file
+ * conditions:
+ *  1. file must .js or .ts filename extension;
+ *
+ */
 function checkIsValidateInModelsFolder(srcDir: string, filePath: string, filename: string): boolean {
+  // models folder
+  // Example: xxx/models/xx.[t|j]s;
   const modelsDir = '/models';
   const isInValidateModelsDir = !!vscode.workspace.workspaceFolders?.find((workspaceFolder) => {
     return (
@@ -47,40 +64,39 @@ function checkIsValidateInModelsFolder(srcDir: string, filePath: string, filenam
   return isInValidateModelsDir && checkIsTsorJsFile(filename);
 }
 
-function getCreateFileType(file: vscode.Uri): createFileTypes {
+function getCreateFileType(file: vscode.Uri): CreateFileTypes {
   const { fsPath } = file;
   const srcDir = 'src';
   const filename = path.basename(fsPath);
   const filePath = path.dirname(fsPath);
   if (checkIsValidateStoreFile(srcDir, filePath, filename)) {
-    return createFileTypes.store;
+    return CreateFileTypes.Store;
   } else if (checkIsValidateInModelsFolder(srcDir, filePath, filename)) {
-    return createFileTypes.inModels;
+    return CreateFileTypes.Models;
   } else {
-    return createFileTypes.null;
+    return CreateFileTypes.Null;
   }
 }
 
-async function checkIsValidate(file: vscode.Uri, projectFramework: string): Promise<[createFileTypes, boolean]> {
+async function checkIsValidate(file: vscode.Uri, projectFramework: string): Promise<[CreateFileTypes, boolean]> {
   if (['rax-app', 'icejs'].includes(projectFramework)) {
     const createFileType = getCreateFileType(file);
-    return [createFileType, createFileType !== createFileTypes.null];
+    return [createFileType, createFileType !== CreateFileTypes.Null];
   }
-  return [createFileTypes.null, false];
+  return [CreateFileTypes.Null, false];
 }
 
 async function fillContent(
   file: vscode.Uri,
   projectFramework: string,
-  createFileType: createFileTypes,
-): Promise<string> {
+  createFileType: CreateFileTypes,
+) {
   const { fsPath: newFsPath } = file;
   const dependency = projectFramework === 'icejs' ? 'ice' : 'rax-app';
-  const templateName = createFileType === createFileTypes.inModels ? 'models' : 'store';
+  const templateName = createFileType === CreateFileTypes.Models ? 'models' : 'store';
   const templatePath = path.join(__dirname, `${templateName}.ts.ejs`);
   const content = await renderFileAsync(templatePath, { dependency });
   await writeFileAsync(newFsPath, content);
-  return newFsPath;
 }
 
 export default async (file: vscode.Uri) => {
@@ -88,7 +104,6 @@ export default async (file: vscode.Uri) => {
   const [createFileType, isValidate] = await checkIsValidate(file, projectFramework);
   if (isValidate) {
     // auto fill content with the type of file;
-    const newFsPath = await fillContent(file, projectFramework, createFileType);
-    vscode.workspace.openTextDocument(newFsPath);
+    await fillContent(file, projectFramework, createFileType);
   }
 };
