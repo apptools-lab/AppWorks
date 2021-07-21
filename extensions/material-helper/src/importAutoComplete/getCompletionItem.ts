7c3e910b5f953e700559ad462ac6912bf8e8a66c
one <one@onedeMacBook-Air.local>
2021-07-21 15:08:56 +0800

    refactor: add commit

--- extensions/material-helper/src/importAutoComplete/getCompletionItem.ts
+++ extensions/material-helper/src/importAutoComplete/getCompletionItem.ts
@@ -6,6 +6,7 @@ export default function getCompletionItem(itemText: string, moduleValue?: string
   // if moduleName is undefined, get it from filename;
   const moduleName = moduleValue || getFilenameWithoutExtname(path.basename(itemText));
   const completionItem = new vscode.CompletionItem(`import ${moduleName} from '${itemText}';`, vscode.CompletionItemKind.Variable);
+  // special deal with {  };
   moduleValue = moduleValue === '{  }' ? '{ ${1} }' : `\${1:${moduleName}}`;
   completionItem.detail = 'AppWorks';
   completionItem.insertText = new vscode.SnippetString(`import ${moduleValue} from '${itemText}';`);
