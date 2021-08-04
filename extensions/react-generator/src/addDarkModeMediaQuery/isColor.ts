4b75dafa762d1ea439b75a788af2ac90bc7dab70
yangfan <18767120422@163.com>
2021-08-04 18:28:27 +0800

    feat: update isColor

--- extensions/react-generator/src/addDarkModeMediaQuery/isColor.ts
+++ extensions/react-generator/src/addDarkModeMediaQuery/isColor.ts
@@ -1,12 +1,12 @@
 import * as csscolors from 'css-color-names';
 
-const colorRegDict = Object.keys(csscolors).concat([
+const colorRegDict = [
   '(#[0-9A-F]{6})',
   '(#[0-9A-F]{3})',
   '(rgb|hsl)a?([^)]*)',
-]);
+];
 const COLOR_REGEXP = new RegExp(colorRegDict.join('|'), 'i');
 
-export default function isColor(vale: string): boolean {
-  return COLOR_REGEXP.test(vale);
+export default function isColor(value: string): boolean {
+  return csscolors[value] || COLOR_REGEXP.test(value);
 }
