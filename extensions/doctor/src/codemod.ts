29fe9444e465f06b340e7b112fff443e748724ad
yangfan <18767120422@163.com>
2021-09-28 14:00:24 +0800

    feat: update (#954)

--- extensions/doctor/src/codemod.ts
+++ extensions/doctor/src/codemod.ts
@@ -18,6 +18,9 @@ const SCAN_OPTIONS = {
 
 export async function runCodemod(transform: string) {
   const result = await doctor.scan(projectPath, Object.assign({ transforms: [transform] }, SCAN_OPTIONS));
+  const { env, window } = vscode;
+  const isEn = env.language === 'en';
+  window.showInformationMessage(`${isEn ? 'Codemod run success, logs shows in the OUTPUT.' : 'Codemod 运行成功，运行日志将在 “输出” 中展示。'}`);
   setOutput(result.codemod?.reports[0].output || '');
   return result;
 }
