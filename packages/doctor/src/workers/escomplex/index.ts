aed0e3fadb89669199c7254878eba9206be47acc
luhc228 <luhengchang228@gmail.com>
2021-12-29 20:30:20 +0800

    chore: remove maintainability detail from report

--- packages/doctor/src/workers/escomplex/index.ts
+++ packages/doctor/src/workers/escomplex/index.ts
@@ -1,13 +1,11 @@
 import * as fs from 'fs-extra';
 import { join } from 'path';
-import getMaintainabilityReports from './getMaintainabilityReports';
 import config from '../../config';
 
 const [tempFileDir] = process.argv.slice(2)[0].split(' ');
 
 function run() {
-  const files = fs.readJSONSync(join(tempFileDir, config.tmpFiles.files));
-  const result = getMaintainabilityReports(files);
+  const result = { score: 0, reports: [] };
 
   fs.writeFileSync(join(tempFileDir, config.tmpFiles.report.escomplex), JSON.stringify(result));
 }
