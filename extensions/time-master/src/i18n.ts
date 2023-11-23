ef2e0dd468e592d95e67c3ba8c9e2d794a751831
Elchin Sarkarov <56814903+Elchinchel@users.noreply.github.com>
2023-11-23 06:26:35 +0300

    feat: add ru-RU locale (#1070)

--- extensions/time-master/src/i18n.ts
+++ extensions/time-master/src/i18n.ts
@@ -2,10 +2,12 @@ import * as vscode from 'vscode';
 import I18nService from '@appworks/i18n';
 import * as zhCNTextMap from './locales/zh-CN.json';
 import * as enUSTextMap from './locales/en-US.json';
+import * as ruRUTextMap from './locales/ru-RU.json';
 
 const i18n = new I18nService();
 i18n.registry('zh-cn', zhCNTextMap);
 i18n.registry('en', enUSTextMap);
+i18n.registry('ru', ruRUTextMap);
 i18n.setLocal(vscode.env.language);
 
 export default i18n;
